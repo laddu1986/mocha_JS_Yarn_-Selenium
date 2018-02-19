3a375185abc63520d7c7e21a784fc1929682bba2
abhidp <abhi358@gmail.com>
2018-02-19 17:00:38 +1100

    gitignore change

---
+++ test/web/specs/signInTest.js
@@ -0,0 +1,44 @@
+import Home from '../page_objects/homePage.js';
+import SignIn from '../page_objects/signInPage.js';
+const config = require('config-yml');
+
+
+describe('Open Sign in page', () => {
+
+    before('On the Amazon home page...', () => {
+
+      console.log(config.api.signIn);
+      SignIn.open(config.api.signIn);
+
+    });
+
+    it('Enters email and password', () => {
+
+      SignIn.emailInput.waitForExist();
+      SignIn.emailInput.waitForVisible();
+      SignIn.emailInput.setValue('avi.pardu.nash3@gmail.com');
+     
+      SignIn.passwordInput.waitForExist();
+      SignIn.passwordInput.waitForVisible();
+      SignIn.passwordInput.setValue('Mob@1234');
+
+      SignIn.signInButton.waitForExist();
+      SignIn.signInButton.waitForVisible();
+      SignIn.signInButton.click();
+
+    });
+
+    it('Shows a positive number of results', () => {
+
+      SignIn.signInMessage.waitForExist();
+      SignIn.signInMessage.waitForVisible();
+      let actual = SignIn.signInMessage.getText();
+      // Results.resultsCount.waitForExist();
+      // Results.resultsCount.waitForVisible();
+      // //regex drags nums out of results text and pushes to an array
+      // const regex = /\d\s*\-\s*(\d+){1}\s*(?:of)?(\s*\d+)?\s*.*/g
+      // const nums = regex.exec(Results.resultsCount.getText());
+      expect(actual).to.equal('Thank you for signing in.');
+    });
+
+});
