fbeef5560074852b7018193985e81849a1d9f66a
avinash.eediga <avinash.eediga@github.com>
2018-02-28 14:15:01 +1100

    no message

--- test/config/wdio.appcurator.js
+++ test/config/wdio.appcurator.js
@@ -1,30 +1,25 @@
 require('dotenv').config();
-var argv = require('yargs').argv;
+const argv = require('yargs').argv;
 // var config = require('config-yml');
 
-let brow, services, user, key, capabilities
-
-(argv.browser) ? (brow = argv.browser) : (brow = 'chrome');
-
-services = ['selenium-standalone'], capabilities = [{
-  browserName: brow,
-  chromeOptions: { args: ['disable-infobars'] }
-}]
+const brow = 'chrome';
 
 exports.config = {
-  services,
+  services: ['selenium-standalone'],
+  capabilities: [{
+    browserName: brow,
+    chromeOptions: { args: ['disable-infobars'] },
+  }],
 
   updateJob: false,
   specs: [
-    './test/web/specs/*.js'
+    './test/web/specs/*.js',
   ],
   // Patterns to exclude.
   exclude: [
     // './test/specs/amazonSearchTest.js',
     // './test/specs/amazonShoppingCart.js'
   ],
-
-  capabilities,
 
   logLevel: 'silent',
   coloredLogs: true,
@@ -34,7 +29,7 @@ exports.config = {
   // maxInstances: 3,
 
   plugins: {
-    'wdio-screenshot': {}
+    'wdio-screenshot': {},
     // webdrivercss: {
     //     screenshotRoot: 'my-shots',
     //     failedComparisonsRoot: 'diffs',
@@ -46,18 +41,18 @@ exports.config = {
   },
 
   framework: 'mocha',
-  reporters: ['spec'/*, 'sumologic'*/],
-  // reporterOptions: {
-  //     sumologic: {
-  //       syncInterval: 100,
-  //       sourceAddress: process.env.SUMO_SOURCE_ADDRESS
-  //     }
-  // },
+  reporters: ['spec'/* , 'sumologic'*/],
+//   reporterOptions: {
+//     sumologic: {
+//       syncInterval: 100,
+//       sourceAddress: process.env.SUMO_SOURCE_ADDRESS,
+//     },
+//   },
 
   mochaOpts: {
     ui: 'bdd',
     compilers: ['js:babel-register'],
-    timeout: 60000
+    timeout: 60000,
   },
 
   //
@@ -66,18 +61,18 @@ exports.config = {
   // =====
   // Gets executed before all workers get launched.
   onPrepare() {
-    console.log("what is the");
+    console.log('what is the');
   },
 
   // Gets executed before test execution begins. At this point you will have access to all global
   // variables like `browser`. It is the perfect place to define custom commands.
   before() {
     const chai = require('chai');
+
     global.expect = chai.expect;
     chai.Should();
 
-    // const config = require('config-yml');   
-
+    // const config = require('config-yml');
   },
 
   // Gets executed after all tests are done. You still have access to all global variables from
@@ -90,5 +85,5 @@ exports.config = {
   // possible to defer the end of the process using a promise.
   onComplete() {
     // do something
-  }
+  },
 };
