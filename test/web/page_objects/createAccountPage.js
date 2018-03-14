5d6f8d1517d68d587ca3032ab7c438d06b3daff7
avinash.eediga <avinash.eediga@github.com>
2018-03-14 11:53:19 +1100

    UI::Register a membership

--- test/web/page_objects/createAccountPage.js
+++ test/web/page_objects/createAccountPage.js
@@ -1,20 +1,19 @@
 import Page from './page';
+
 const config = require('config-yml');
+
 class CreateAccountPage extends Page {
+  get emailInput() { return browser.element(config.web.emailInput); }
+  get passwordInput() { return browser.element(config.web.passwordInput); }
+  get nameInput() { return browser.element(config.web.nameInput); }
+  get organizationInput() { return browser.element(config.web.organizationInput); }
+  get createAccountButton() { return browser.element(config.web.createAccountButton); }
+  get signInMessage() { return browser.element(config.web.signInMessage); }
 
-    get emailInput ()  { return browser.element(config.web.emailInput); }
-    get passwordInput()  { return browser.element(config.web.passwordInput); }
-    get nameInput()  { return browser.element(config.web.nameInput); }
-    get organisationInput()  { return browser.element(config.web.organisationInput); }
-    get createAccountButton()  { return browser.element(config.web.createAccountButton); }
-    get signInMessage()  { return browser.element(config.web.signInMessage); }
-    
-    open(e) {
-        console.log(e);
-        super.open(e);
-    } 
-
-
+  open(e) {
+    console.log(e);
+    super.open(e);
+  }
 }
 
 export default new CreateAccountPage();
