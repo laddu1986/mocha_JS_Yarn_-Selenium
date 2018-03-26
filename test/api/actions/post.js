5293a170af07de22f19eb2b0cc5779a3d9e77612
Abhi Daspatnaik <abhijeet.daspatnaik@massive.co>
2018-03-26 11:16:23 +1100

    orca test

--- test/api/actions/post.js
+++ test/api/actions/post.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import * as lib from '../../common';
 
 class Post {
@@ -8,17 +7,6 @@ class Post {
     const response = lib.chakram.post(params[0], params[1]);
     return response;
   }
-=======
-import { chakram } from '../../common';
-class Post  {
-    
-    //returns delete response object
-    //params = url
-	post(params) {
-        const response = chakram.post(params[0],params[1]);
-		return response;
-	}
->>>>>>> 0d0d9f47265e965904031d8f79bde1953bcceaeb
 }
 
 export default new Post();
--- test/common.js
+++ test/common.js
@@ -3,14 +3,10 @@ import assertions from './api/actions/assertions';
 import del from './api/actions/delete';
 import post from './api/actions/post';
 
-<<<<<<< HEAD
-const server = require('chakram');
-=======
 const chakram = require('chakram');
 var cookieParser = require('cookie-parser');
->>>>>>> 0d0d9f47265e965904031d8f79bde1953bcceaeb
 
-global.expect = server.expect;
+global.expect = chakram.expect;
 const mysql = require('mysql');
 const config = require('config-yml');
 const faker = require('faker');
@@ -49,8 +45,4 @@ function end() {
 
 // con.end();
 
-<<<<<<< HEAD
-export { assertions, faker, config, server, del, post, connection, end };
-=======
 export { assertions, faker, config, chakram, del, post, connection, end, cookieParser };
->>>>>>> 0d0d9f47265e965904031d8f79bde1953bcceaeb
