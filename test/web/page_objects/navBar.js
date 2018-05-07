a010f25657a600dc6552d12b3150bb60e7e7ace9
abhidp <abhi358@gmail.com>
2018-05-07 14:34:53 +1000

    Automation tests for invite functionality

--- test/web/page_objects/navBar.js
+++ test/web/page_objects/navBar.js
@@ -2,15 +2,12 @@ import Page from './page';
 
 class NavBar extends Page {
 
-
-
-
-
-
-
-
-  //Help
+//settings
+ get settingsAnchor() { return browser.element("//*[@data-qa='nav:org-settings']"); }
+ get backToOrgDashboardLink() { return browser.element("//a[@data-qa='link:dashboard']") }
+ //Help
   get helpMenuNav() { return browser.element("//*[@data-qa='menu:help']"); }
+  get teamNavButton() { return browser.element("//*[@data-qa='link:team']"); }
   get helpCenterAnchor() { return browser.element("//*[contains(text(),'Help Center')]"); }
   get devPortalAnchor() { return browser.element("//*[contains(text(),'Developer Portal')]"); }
   get apiPortalAnchor() { return browser.element("//*[contains(text(),'API Portal')]"); }
@@ -22,3 +19,4 @@ class NavBar extends Page {
     super.open(e);
   }
 }
+export default new NavBar();
