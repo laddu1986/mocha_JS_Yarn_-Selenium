8eff92fe872db4c583215783445e1c48f50d4ae7
abhidp <abhi358@gmail.com>
2018-05-08 10:29:21 +1000

    Automation for Copy API Key to clipboard.
    changing apiKey locator

--- test/web/page_objects/spaceDashboardPage.js
+++ test/web/page_objects/spaceDashboardPage.js
@@ -1,9 +1,11 @@
 import Page from './page';
 
-
 class SpaceDashboardPage extends Page {
   get currentSpaceName() { return browser.element("//section[@data-qa='notifications:passive']") }
   get devApiGuideButton() { return browser.element("//div[contains(text(),'Developer API Guide')]") }
+  get APIKey() { return browser.element("//p[@data-qa='api-key-value']") }
+  get copyToClipboard() { return browser.element("//button[@title='Copy']") }
+  get successMsg() { return browser.element("//div[@data-qa='notification:success']") }
 
   open(e) {
     super.open(e);
