3db15a5eceb20d01b10ad537f8c763b2b01a3d01
abhidp <abhi358@gmail.com>
2018-05-08 10:29:21 +1000

    Automation for Copy API Key to clipboard.

--- test/web/actions/createSpace.js
+++ test/web/actions/createSpace.js
@@ -1,18 +1,49 @@
-import OrgDashboardPage from '../page_objects/orgDashboardPage'
-import { setValue, click, waitForEnabled, waitForElement } from '../actions/actions'
+import OrgDashboardPage from '../page_objects/orgDashboardPage';
+import { setValue, click, waitForEnabled, waitForElement } from '../actions/actions';
 import * as lib from '../../common';
 import SpaceDashboardPage from '../page_objects/spaceDashboardPage';
+const copyPasteModule = require('copy-paste');
 
-
-function createFirstSpace() {
-  setValue(OrgDashboardPage.createSpaceInput, lib.testData.space)
-  click(OrgDashboardPage.createSpaceButton)
-  waitForElement(SpaceDashboardPage.devApiGuideButton)
+function createSpace() {
+  setValue(OrgDashboardPage.createSpaceInput, lib.testData.space);
+  click(OrgDashboardPage.createSpaceButton);
 }
 
-function createAnotherSpace() {
-  click(OrgDashboardPage.createNewSpaceButton)
-  createFirstSpace()
+function verifySpace() {
+  waitForElement(SpaceDashboardPage.devApiGuideButton);
+  return SpaceDashboardPage.devApiGuideButton.isVisible();
 }
 
-export { createFirstSpace, createAnotherSpace }
+function clickCreateNewSpaceButton() {
+  click(OrgDashboardPage.createNewSpaceButton);
+}
+
+function goBackToSpacesPage() {
+  const goBackToOrg = (lib.config.api.base + '/' + lib.testData.organization).toLowerCase() + '/';
+  browser.url(goBackToOrg); //Temporary - will change when we have a "Go Back to OrgDashBoard" link in the space page
+  waitForElement(OrgDashboardPage.spaceCards);
+}
+
+function createSpaceButtonEnabled() {
+  return OrgDashboardPage.createSpaceButton.isEnabled();
+}
+
+function copyAPIKeyToClipBoard() {
+  SpaceDashboardPage.copyToClipboard.click();
+}
+
+function copyToastMessage() {
+  waitForElement(SpaceDashboardPage.successMsg);
+  return SpaceDashboardPage.successMsg.getText();
+}
+
+function copiedValue() {
+  var copiedValue =copyPasteModule.paste();
+  return copiedValue;
+}
+
+function defaultAPIKey() {
+  return SpaceDashboardPage.APIKey.getText();
+}
+
+export { goBackToSpacesPage, defaultAPIKey, copiedValue, createSpace, verifySpace, clickCreateNewSpaceButton, createSpaceButtonEnabled, copyAPIKeyToClipBoard, copyToastMessage }
--- test/web/page_objects/spaceDashboardPage.js
+++ test/web/page_objects/spaceDashboardPage.js
@@ -1,10 +1,12 @@
 import Page from './page';
 
-
 class SpaceDashboardPage extends Page {
   get currentSpaceName() { return browser.element("//section[@data-qa='notifications:passive']") }
   get devApiGuideButton() { return browser.element("//div[contains(text(),'Developer API Guide')]") }
-
+  get APIKey() { return browser.element("//div[@data-qa='page:space-dashboard']//section[2]/p") }
+  get copyToClipboard() { return browser.element("//button[@title='Copy']") } 
+  get successMsg() { return browser.element("//div[@data-qa='notification:success']") }
+  
   open(e) {
     super.open(e);
   }
