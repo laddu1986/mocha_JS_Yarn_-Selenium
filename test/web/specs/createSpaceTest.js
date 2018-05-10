bed6ecb99e2676d72132f8460c607c256657ec36
Balpreet Kaur <balpreet.kaur@massive.co>
2018-05-10 14:06:18 +1000

    adding data folder for json files and changing per comments

--- test/web/specs/createSpaceTest.js
+++ test/web/specs/createSpaceTest.js
@@ -1,8 +1,9 @@
 import { createAccount } from '../actions/createAccount';
 import { signOut } from '../actions/signOut';
 import * as createSpaceActions from '../actions/createSpace';
-import * as lib from '../../common';
+import * as commonActions from '../actions/actions';
 import SignInPage from '../page_objects/signInPage';
+import spaceData from '../data/space.json';
 
 describe('Space Tests', () => {
   before('Open App URL', () => {
@@ -10,33 +11,41 @@ describe('Space Tests', () => {
     createAccount();
   });
 
-    it('Verify Space Button', () => {
-      expect(createSpaceActions.createSpaceButtonEnabled()).to.equal(false);
-    });
+  it('Verify Space Button', () => {
+    expect(createSpaceActions.createSpaceButtonEnabled()).to.equal(false);
+  });
 
-    it('Verify creating first Space', () => {
+  it('Verify creating first Space', () => {
+    createSpaceActions.createSpace();
+    expect(createSpaceActions.verifySpace()).to.equal(true);
+  });
+
+  it('Copy APIKey --> verify key is copied', () => {
+    createSpaceActions.copyAPIKeyToClipBoard();
+    expect(commonActions.getNotificationMessageText()).to.include(spaceData.copyNotificationMessage.text);
+    expect(createSpaceActions.copiedValue()).to.deep.equal(createSpaceActions.defaultAPIKey());
+  })
+
+  it('Verify creating two more spaces ', () => {
+    for (var i = 0; i < 2; i++) {
+      createSpaceActions.goBackToOrgDashboard();
+      createSpaceActions.clickCreateNewSpaceButton();
       createSpaceActions.createSpace();
-      expect(createSpaceActions.verifySpace()).to.equal(true);
-    });
+      createSpaceActions.verifySpace();
+    }
+  })
 
-    it('Copy APIKey --> verify key is copied', () => {
-      createSpaceActions.copyAPIKeyToClipBoard(); 
-      expect(createSpaceActions.copyToastMessage()).to.include("Copied to clipboard");
-      expect(createSpaceActions.copiedValue()).to.deep.equal(createSpaceActions.defaultAPIKey());
-    })
+  xit('Sorting of Space cards stack', () => {
+    //TO BE IMPLEMENTED WHEN E2E wiring is complete
+  });
 
-    it('Verify creating two more spaces ', () => {
-      for(var i=0;i<2;i++){
-        createSpaceActions.goBackToSpacesPage();
-        createSpaceActions.clickCreateNewSpaceButton();
-        createSpaceActions.createSpace();
-        createSpaceActions.verifySpace();
-      }
-    })
+  xit('Delete Space', () => {
 
-    xit('Last created Space should be at the top of the Space cards stack', () => {     
-      //TO BE IMPLEMENTED WHEN E2E wiring is complete
-    })
+  });
+
+  xit('Edit Space', () => {
+
+  });
 
   after('Sign Out', () => {
     signOut();
