2401092abdc5cdea7afa1e17b2a814f254e4457e
abhidp <abhi358@gmail.com>
2018-05-18 09:53:49 +1000

    Finishing Refactor for remaining API Test Automation specs

--- test/api/actions/invites.js
+++ test/api/actions/invites.js
@@ -5,70 +5,47 @@ function loop() {
   let i;
   for (i = 0; i < 2; i++) {
     array.push(`${lib.bigName(5)}@test.co`);
-    // console.log(array);
   }
   return array;
 }
-function postInvitesByOrganizationId(done, responseData) {
+
+function postInvitesByOrganizationId(responseData) {
   const array = loop();
-  // console.log(array);
-  // adding emails to response array at position 1
-  // responseData.push(array);
   const any = {
     api: `${lib.config.api.organizations + responseData[1].id}/invites`,
-    data: array,
-    func(response) {
-      // responseData.push(response.body);
-      console.log(response.body);
-      expect(response).to.have.status(201);
-    }
+    data: [`${array}`]
   };
-  console.log(any);
-  lib.post(done, any);
+  return lib.post(any);
 }
 
+function getInvitesDetailsByOrganizationId(responseData) {
+  const any = {
+    api: lib.config.api.organizations,
+    data: `${responseData[1].id}/invites?pageSize=2`
+  };
+  return lib.get(any).then((response) => {
+    responseData.push(response.body);
+    return response;
+  })
+}
+function getInvitesByOrganizationId(responseData) {
+  const any = {
+    api: lib.config.api.organizations,
+    data: `${responseData[1].id}/invites`
+  };
+  return lib.get(any);
+}
+function deleteInviteByOrganizationIdAndEmail(responseData) {
+  const any = {
+    api: lib.config.api.organizations,
+    data: `${responseData[1].id}/invites/?email=${responseData[2][0]}`
+  };
+  return lib.del(any);
+}
 
-function getInvitesDetailsByOrganizationId(done, responseData) {
-  const any = {
-    api: lib.config.api.organizations,
-    data: `${responseData[1].id}/invites?pageSize=2`,
-    func(response) {
-      responseData.push(response.body);
-      console.log(response.body);
-      expect(response).to.have.status(200);
-    }
-  };
-  // console.log(any);
-  lib.get(done, any);
-}
-function getInvitesByOrganizationIdAndToken(done, responseData) {
-  // console.log(JSON.stringify(responseData));
-  // console.log(responseData[2].results[0].token);
-  const any = {
-    api: lib.config.api.organizations,
-    data: `${responseData[1].id}/invites/${responseData[2].results[0].token}`,
-    func(response) {
-      responseData.push(response.body);
-      //   console.log(responseData);
-      expect(response).to.have.status(200);
-    }
-  };
-  lib.get(done, any);
-}
-function deleteInviteByOrganizationIdAndEmail(done, responseData) {
-  // console.log(responseData[1][0] +':::;:'+responseData[0].id );
-  const any = {
-    api: lib.config.api.organizations,
-    data: `${responseData[1].id}/invites/?email=${responseData[2][0]}`,
-    func(response) {
-      expect(response).to.have.status(204);
-    }
-  };
-  lib.del(done, any);
-}
 export {
   postInvitesByOrganizationId,
-  getInvitesByOrganizationIdAndToken,
+  getInvitesByOrganizationId,
   getInvitesDetailsByOrganizationId,
   deleteInviteByOrganizationIdAndEmail
 };
--- test/api/specs/spacesTest.js
+++ test/api/specs/spacesTest.js
@@ -4,46 +4,35 @@ import * as identity from '../actions/identity';
 import * as membership from '../actions/membership';
 import * as lib from '../../common';
 
-describe('Identity Api', () => {
-  before('Connect to database', () => {
-    // lib.connection({
-    //   host: 'dev-nextdb.cdiceoz5vyus.ap-southeast-2.rds.amazonaws.com',
-    //   user: 'rouser',
-    //   password: 'R34d0nlyK3y',
-    //   database: 'membership_test',
-    // });
-  });
-  describe('POST /org/{org-id}/space', () => {
-    it('Posting idenity details', (done) => {
-      identity.postIdentity(done, lib.responseData.spaces);
-    });
+var postResponse, getResponse;
 
-    it('Posting organization details', (done) => {
-      organization.postOrganization(done, lib.responseData.spaces);
+describe('Spaces Api', () => {
+  describe('POST /organizations/{orgId}/spaces', () => {
+    before((done) => {
+      identity.postIdentity(lib.responseData.spaces).then(() => {
+        organization.postOrganization(lib.responseData.spaces).then(() => {
+          postResponse = spaces.postSpaceByOrganizationId(lib.responseData.spaces);
+          done();
+        })
+      })
     });
-    // it('Posting membership details', (done) => {
-    //   membership.postMembership(done, lib.responseData.spaces);
-    // });
-    it('Create a new space.', (done) => {
-      spaces.postSpaceByOrganizationId(done, lib.responseData.spaces);
-    });
-  });
-  // describe('POST /org/{org-id}/space/key', () => {
-  //   it('Create a new space and a key.', (done) => {
-  //     identity.postIdentity(done, lib.responseData.identity);
-  //   });
-  // });
-  describe('GET /org/{org-id}/space', () => {
-    it('Get a space by ID.', (done) => {
-      spaces.getSpacesByOrganizationId(done, lib.responseData.spaces);
+    it('Create a new space.', () => {
+      return postResponse.then((response) => {
+        expect(response).to.have.status(201);
+      })
     });
   });
-  // describe('GET /org/{org-id}/space/{spaceId}', () => {
-  //   it('Get All Spaces for an Organization.', (done) => {
-  //     identity.getIdentityById(done, lib.responseData.identity);
-  //   });
-  // });
-  after('End message', () => {
-    // lib.end();
+
+  describe('GET /organizations/{orgId}/spaces', () => {
+    before((done) => {
+      getResponse = spaces.getSpacesByOrganizationId(lib.responseData.spaces);
+      done();
+    })
+    it('Get All Spaces for an Organization', () => {
+      return getResponse.then((response) => {
+        expect(response).to.have.status(200);
+      })
+    });
   });
+
 });
