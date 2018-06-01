aa5f389f2a7b03e4f293c1b6c67c00f0a5402c67
Balpreet Kaur <balpreet.kaur@massive.co>
2018-06-01 11:58:20 +1000

    F/E automation: Capability to pass browser names from arguments at runtime
    Changes for deleteAccountTest

--- test/web/actions/common.js
+++ test/web/actions/common.js
@@ -3,31 +3,24 @@ import CommonPage from '../page_objects/common';
 import { click, waitForElement, setValue } from '../actions/actions';
 import SignInPage from '../page_objects/signInPage'
 
-function getNotificationMessageText() {
+export function getNotificationMessageText() {
     waitForElement(CommonPage.successMsg);
     return CommonPage.successMsg.getText();
 }
 
-function closePassiveNotification() {
+export function closePassiveNotification() {
     click(CommonPage.dismissNotification)
 }
 
-function signOut() {
+export function signOut() {
     click(NavBar.profileMenu);
     click(NavBar.signOut);
     waitForElement(CommonPage.submitButton);
 }
 
-function signIn(email, password) {
+export function signIn(email, password) {
     setValue(SignInPage.emailInput, email);
     setValue(SignInPage.passwordInput, password);
     click(CommonPage.submitButton);
     waitForElement(NavBar.profileMenu);
 }
-
-export {
-    getNotificationMessageText,
-    closePassiveNotification,
-    signOut,
-    signIn
-};
