090d657c73cf02f23de3866240ec695c3a34f584
Abhi Daspatnaik <abhijeet.daspatnaik@massive.co>
2018-06-07 16:04:50 +1000

    invites

---
+++ test/web/specs/organizations/createOrganizationTest.js
@@ -0,0 +1,36 @@
+// Create Organization, sign out, sign back in to validate user lands in the created Org
+import * as lib from '../../../common';
+import { createAccount } from 'web/actions/account';
+import { signOut, signIn } from 'web/actions/common';
+import { verifyOrgDashBoardAfterLogin, goToCreateOrgPageFromNavbar, verifyCreateOrgPage, createNewOrg, verifyOrgIsCreated } from 'web/actions/organization';
+import { getnavOrgCount } from 'web/actions/navBar';
+import SignInPage from 'web/page_objects/signInPage';
+var orgName = lib.randomString.generate(10), accountDetails;
+
+describe('Tests for Create Organization', () => {
+  before(() => {
+    SignInPage.open();
+    accountDetails = createAccount();
+  });
+
+  it(`\nGo to organization creation page\n`, () => {
+    goToCreateOrgPageFromNavbar();
+    expect(verifyCreateOrgPage()).to.equal(true);
+  });
+
+  it('Create new organization', () => {
+    createNewOrg(orgName);
+    expect(verifyOrgIsCreated()).to.equal(true);
+    expect(browser.getUrl()).to.include(orgName.toLowerCase());
+  });
+
+  it(`\nSign out and back in -->Should show last accessed Org dashboard`, () => {
+    signOut();
+    signIn(accountDetails.email, 'Pass1234');
+    expect(verifyOrgDashBoardAfterLogin()).to.equal(orgName);
+  });
+
+  it(`\n Checking Org Count in Navbar`, () => {
+    expect(getnavOrgCount()).to.equal(2);
+  })
+});
---
+++ test/web/specs/organizations/updateOrganizationNameTest.js
@@ -0,0 +1,83 @@
+
+//import createOrganizationPage from '../specs/createOrganizationTest';
+import * as lib from '../../../common';
+import NavBar from 'web/page_objects/navBar'
+import { createAccount } from 'web/actions/account'
+import SettingsPage from 'web/page_objects/settingsPage';
+import HomePage from 'web/page_objects/homePage';
+import OrgDashboardPage from 'web/page_objects/orgDashboardPage';
+import SignInPage from 'web/page_objects/signInPage';
+import { createOrg } from 'web/actions/organization';
+let updatedOrgName;
+let orgname
+
+describe('Create Account', () => {
+
+  before('Open App URL', () => {
+    SignInPage.open();
+  });
+
+  it('Create Account', () => {
+    createAccount();
+  });
+
+  it('Create two more Orgs', () => {
+    let i;
+    for (i = 0; i < 3; i++) {
+      orgname = lib.randomString.generate(10);
+      createOrg(orgname);
+      i += 1;
+    }
+  });
+
+});
+
+describe('Update Organization name', () => {
+  it('Click Profile', () => {
+    click(HomePage.profileMenu);
+  });
+
+  it('Click Settings', () => {
+    click(HomePage.settingsAnchor);
+    expect(browser.getUrl()).to.include((`${lib.web}/${orgname}/edit`).toLowerCase())
+  });
+
+  it('Change the org name to "Updated Organization"', () => {
+    waitForVisible(SettingsPage.orgInput);
+    SettingsPage.orgInput.clearElement();
+
+    updatedOrgName = 'Updated Organization';
+
+    setValue(SettingsPage.orgInput, updatedOrgName);
+
+    waitForEnabled(SettingsPage.saveOrgNameButton);
+    expect(SettingsPage.saveOrgNameButton.isEnabled()).to.equal(true);
+    click(SettingsPage.saveOrgNameButton);
+  });
+
+  it('Should update the side nav bar with the updated org name', () => {
+    browser.waitUntil(() => NavBar.backToOrgDashboardLink.getText() === SettingsPage.orgInput.getValue(), 5000, 'Expect orgname to change in the side nav bar', 200);
+    expect(NavBar.backToOrgDashboardLink.getText()).to.equal(SettingsPage.orgInput.getValue());
+  });
+
+  it('Go back to Org Dashboard from the side nav bar org link', () => {
+    click(NavBar.backToOrgDashboardLink);
+  });
+
+  it('Validate Org dashboard has the updated org name', () => {
+    const orgDashboardOrgName = OrgDashboardPage.currentOrgName.getText();
+    expect(orgDashboardOrgName).to.equal(updatedOrgName);
+  });
+
+  it('Go back to Choose org page', () => {
+    browser.url(`${lib.web}/organizations`)
+    waitForVisible(HomePage.chooseOrg);
+    expect(HomePage.chooseOrg.isVisible()).to.equal(true);
+  });
+
+  it('Modified org should be at the top of the org card stack', () => {
+    waitForVisible(HomePage.orgCards);
+    const topOrgCard = HomePage.orgCards.getText()[0];
+    expect(topOrgCard).to.include(updatedOrgName);
+  });
+});
