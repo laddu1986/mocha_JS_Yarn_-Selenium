d5e0dfd2c89605d128b7f692e7203275853e7f8e
Abhi Daspatnaik <abhijeet.daspatnaik@massive.co>
2018-07-02 09:53:38 +1000

    docker

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,20 @@
-export DISPLAY=:99
-Xvfb :99 -shmem -screen 0 1366x768x16 &
-x11vnc -passwd secret -display :99 -N -forever &
-# google-chrome-stable --remote-debugging-port=9222 &
-# selenium must be started by a non-root user otherwise chrome can't start
-su - seleuser -c "selenium-standalone start &"
+# export DISPLAY=:99
+# Xvfb :99 -shmem -screen 0 1366x768x16 &
+# x11vnc -passwd secret -display :99 -N -forever &
+# # google-chrome-stable --remote-debugging-port=9222 &
+# # selenium must be started by a non-root user otherwise chrome can't start
+# su - seleuser -c "selenium-standalone start &"
+
+
+
+#NEW
+source /functions.sh
+
+export GEOMETRY="$SCREEN_WIDTH""x""$SCREEN_HEIGHT""x""$SCREEN_DEPTH"
+
+rm -f /tmp/.X*lock
+
+SERVERNUM=$(get_server_num)
+xvfb-run -n $SERVERNUM --server-args="-screen 0 $GEOMETRY -ac +extension RANDR" &
+
+su - seluser -c "selenium-standalone start &"
