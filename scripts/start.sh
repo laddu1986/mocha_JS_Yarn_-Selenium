e90ce17d95cda9e516febc467b86cda267950506
abhidp <abhi358@gmail.com>
2018-07-03 02:47:55 +0000

    start.sh edited online with Bitbucket

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 export DISPLAY=:99
 Xvfb :99 -shmem -screen 0 1366x768x16 &
-x11vnc -ncache_cr -nosetprimary -passwd secret -display :99 -N -forever &
+x11vnc -ncache_cr -noxdamage -noprimary -passwd secret -display :99 -N -forever &
 # google-chrome-stable --remote-debugging-port=9222 &
 # selenium must be started by a non-root user otherwise chrome can't start
 su - seleuser -c "selenium-standalone start &"
