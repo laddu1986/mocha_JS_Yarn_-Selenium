3ab75f7ddf2402d03236a7ee07cbf13f4aab800e
abhidp <abhi358@gmail.com>
2018-07-30 16:38:27 +1000

    UI Automation:  Delete User cases
    commit2
    
    commit3
    
    Changing locators.

--- test/web/page_objects/spaceDashboardPage.js
+++ test/web/page_objects/spaceDashboardPage.js
@@ -1,4 +1,8 @@
 class SpaceDashboardPage {
     get goToAppSettings() { return browser.element("//*[@data-qa='btn:app-settings']"); }
+    get totalUsersCount() { return browser.element("//div[@data-qa='metric:total']"); }
+    get totalVisitorsCount() { return browser.element("//div[@data-qa='metric:visitors']"); }
+    get totalActiveCount() { return browser.element("//article[@data-qa='metric:currentlyActive']//div//div//div"); }
+    get totalNewCount() { return browser.element("//article[@data-qa='metric:newlyJoined']//div//div//div"); }
 }
 export default new SpaceDashboardPage();
---
+++ test/web/specs/metrics/deleteUserTest.js
@@ -0,0 +1,54 @@
+import * as lib from '../../../common';
+import AccountPage from 'web/page_objects/accountPage';
+import { createAccount } from 'web/actions/account';
+import { createSpace, goToAPIKeyPage, defaultAPIKey } from 'web/actions/space';
+import { backToSpaceDashboard, clickOnAudienceLink, clickOnSpaceDashboardLink } from 'web/actions/navBar';
+import { addUsers, addVisitor, getCount } from 'web/actions/metrics';
+import NotificationData from 'web/data/passiveNotification.json'
+import { clickOnUsersTab, clickFirstRow, deleteUser, getFirstRowUserName } from 'web/actions/users';
+import { getNotificationMessageText } from 'web/actions/common';
+var apiKey;
+describe('Delete User Test', () => {
+    before(() => {
+        AccountPage.open();
+        createAccount();
+        createSpace();
+        goToAPIKeyPage();
+        apiKey = defaultAPIKey();
+    });
+
+    before(async () => {
+        await addUsers(2, apiKey);
+    })
+
+    it('Delete User --> Verify users tab and passive notification shows', () => {
+        backToSpaceDashboard();
+        clickOnAudienceLink();
+        clickOnUsersTab();
+        var deletedName = getFirstRowUserName();
+        clickFirstRow();
+        browser.pause(1000);
+        deleteUser();
+        expect(getNotificationMessageText()).to.include(`${NotificationData.deleteMessage.text}\'${deletedName}`);
+        expect(getFirstRowUserName()).to.not.include(deletedName);
+    });
+
+    it('Adding the visitors ', async () => {
+        await addVisitor(3, apiKey);
+    })
+
+    it('Delete Visitor --> Verify users tab and passive notification shows', () => {
+        browser.refresh();
+        clickFirstRow();
+        browser.pause(1000);
+        deleteUser();
+        expect(getNotificationMessageText()).to.include(`${NotificationData.deleteMessage.text}\'Visitor`);
+    });
+
+    it('Verify the count on Space Dashboard', () => {
+        clickOnSpaceDashboardLink();
+        browser.pause(1500);
+        expect(getCount(Constants.UserType.User)).to.include(1);
+        expect(getCount(Constants.UserType.Visitor)).to.include(2);
+    });
+});
