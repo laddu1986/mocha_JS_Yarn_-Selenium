7f968d72742a00d339f287f8f713c0e360e08154
abhidp <abhi358@gmail.com>
2018-08-14 15:11:03 +1000

    label suggestions

--- test/web/page_objects/common.js
+++ test/web/page_objects/common.js
@@ -9,9 +9,9 @@ class common extends Page {
     get intercomIcon() { return browser.element('#intercom-frame') }
 
     //Passive Notifications
-    get successMsg() { return browser.element("//div[@data-qa='notification:success']") }
-    get errorMsg() { return browser.element("//div[@data-qa='notification:error']") }
-    get dismissNotification() { return browser.element("//button[@data-qa='btn:dismiss']") }
+    get successMsg() { return browser.element("//section[@data-qa='notifications:passive']//div[@data-qa='notification:success']") }
+    get errorMsg() { return browser.element("//section[@data-qa='notifications:passive']//div[@data-qa='notification:error']") }
+    get dismissNotification() { return browser.element("//section[@data-qa='notifications:passive']//button[@data-qa='btn:dismiss']") }
 
     //Page messages
     get invalidInvitationMsg() { return browser.element("//h1") }
