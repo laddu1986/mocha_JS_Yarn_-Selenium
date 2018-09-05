70cf72e0517012fa3256c108263ee4a0dd58f05b
abhidp <abhi358@gmail.com>
2018-09-05 17:53:26 +1000

    grpc

--- test/grpc/specs/segment/segmentTest.js
+++ test/grpc/specs/segment/segmentTest.js
@@ -4,7 +4,6 @@ const caller = require('grpc-caller')
 
 //setup assertion library
 var chai = require('chai')
-
 var expect = chai.expect
 
 //define proto file location and port# of Service running locally inside K8s cluster
@@ -23,12 +22,13 @@ describe('Segment Service', () => {
 
 
   it('GET  /getSegmentById', async () => {
-    return client.getSegmentById({ segmentContext: { orgId: '37c9ad00-855b-4882-b33a-f3d9340aa4de', spaceId: '1f78da01-b1ac-4ac3-8952-bd646c1fbe6e', segmentId: '0rvl39E' } })
+    await client.getSegmentById({ segmentContext: { orgId: '37c9ad00-855b-4882-b33a-f3d9340aa4de', spaceId: '1f78da01-b1ac-4ac3-8952-bd646c1fbe6e', segmentId: '0rvl39E' } })
       .then((response) => {
         expect(response).to.be.an('object')
-        expect(Object(response).title).to.deep.equal('MY-SEGMENT')
-        expect(Object(response).tagline).to.deep.equal('my tagline')
+        expect(response.title).to.deep.equal('MY-SEGMENT')
+        expect(response.tagline).to.deep.equal('my tagline')
       })
+
   })
 
 })
