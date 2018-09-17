6549c08f7c4f4a0c9fa4f380f3024375c26070df
Balpreet Kaur <balpreet.kaur@massive.co>
2018-09-18 09:40:23 +1000

    commit-1 (Tribe Card Customise tests)
    adding tribe Modal UI cases

--- test/web/specs/metrics/deleteUserTest.js
+++ test/web/specs/metrics/deleteUserTest.js
@@ -1,55 +1,55 @@
-import * as lib from '../../../common';
+import '../../../common';
 import AccountPage from 'web/page_objects/accountPage';
 import { createAccount } from 'web/actions/account';
-import { createSpace, goToDeveloperPortal, defaultAPIKey } from 'web/actions/space';
+import { createSpace, goToDeveloperPortal, getAPIKey } from 'web/actions/space';
 import { clickOnAudienceLink, clickOnSpaceDashboardLink } from 'web/actions/navBar';
 import { addUsers, addVisitor, getCount } from 'web/actions/metrics';
-import NotificationData from 'web/data/passiveNotification.json'
+import NotificationData from 'web/data/passiveNotification.json';
 import { clickOnUsersTab, clickFirstRow, deleteUser, getFirstRowDetails } from 'web/actions/users';
 import { getNotificationMessageText } from 'web/actions/common';
-import Constants from 'data/constants.json'
+import Constants from 'data/constants.json';
 
 var apiKey;
 describe('Delete User Test', () => {
-    before(() => {
-        AccountPage.open();
-        createAccount();
-        createSpace();
-        goToDeveloperPortal();
-        apiKey = defaultAPIKey();
-    });
+  before(() => {
+    AccountPage.open();
+    createAccount();
+    createSpace();
+    goToDeveloperPortal();
+    apiKey = getAPIKey();
+  });
 
-    before(async () => {
-        await addUsers(2, apiKey);
-    })
+  before(async () => {
+    await addUsers(2, apiKey);
+  });
 
-    it('Delete User --> Verify users tab and passive notification shows', () => {
-        clickOnAudienceLink();
-        clickOnUsersTab();
-        var deletedName = getFirstRowDetails(Constants.UserAttributes.Name);
-        clickFirstRow();
-        browser.pause(1000);
-        deleteUser();
-        expect(getNotificationMessageText()).to.include(`${NotificationData.deleteMessage.text}\'${deletedName}`);
-        expect(getFirstRowDetails(Constants.UserAttributes.Name)).to.not.include(deletedName);
-    });
+  it('Delete User --> Verify users tab and passive notification shows', () => {
+    clickOnAudienceLink();
+    clickOnUsersTab();
+    var deletedName = getFirstRowDetails(Constants.UserAttributes.Name);
+    clickFirstRow();
+    browser.pause(1000);
+    deleteUser();
+    expect(getNotificationMessageText()).to.include(`${NotificationData.deleteMessage.text}'${deletedName}'`);
+    expect(getFirstRowDetails(Constants.UserAttributes.Name)).to.not.include(deletedName);
+  });
 
-    it('Adding the visitors ', async () => {
-        await addVisitor(3, apiKey);
-    })
+  it('Adding the visitors ', async () => {
+    await addVisitor(3, apiKey);
+  });
 
-    it('Delete Visitor --> Verify users tab and passive notification shows', () => {
-        browser.refresh();
-        clickFirstRow(Constants.UserType.Visitor);
-        browser.pause(1000);
-        deleteUser();
-        expect(getNotificationMessageText()).to.include(`${NotificationData.deleteMessage.text}\'Visitor`);
-    });
+  it('Delete Visitor --> Verify users tab and passive notification shows', () => {
+    browser.refresh();
+    clickFirstRow(Constants.UserType.Visitor);
+    browser.pause(1000);
+    deleteUser();
+    expect(getNotificationMessageText()).to.include(`${NotificationData.deleteMessage.text}'Visitor'`);
+  });
 
-    it('Verify the count on Space Dashboard', () => {
-        clickOnSpaceDashboardLink();
-        browser.pause(1500);
-        expect(getCount(Constants.UserType.User)).to.include(1);
-        expect(getCount(Constants.UserType.Visitor)).to.include(2);
-    });
+  it('Verify the count on Space Dashboard', () => {
+    clickOnSpaceDashboardLink();
+    browser.pause(1500);
+    expect(getCount(Constants.UserType.User)).to.include(1);
+    expect(getCount(Constants.UserType.Visitor)).to.include(2);
+  });
 });
--- test/web/specs/spaces/spaceKeyTest.js
+++ test/web/specs/spaces/spaceKeyTest.js
@@ -1,40 +1,52 @@
-import * as lib from '../../../common';
+import '../../../common';
 import accountPage from 'web/page_objects/accountPage';
 import { createAccount } from 'web/actions/account';
-import { defaultAPIKey, ifIconsEnabled, revokeButtonAppears, verifyAPIKeyStatus, clickUndoButton, clickRevokeButton, createSpace, goToAPIKeyPage, copyAPIKeyToClipBoard, copiedValue, deleteAPIKey } from 'web/actions/space';
+import {
+  getAPIKey,
+  ifIconsEnabled,
+  revokeButtonAppears,
+  verifyAPIKeyStatus,
+  clickUndoButton,
+  clickRevokeButton,
+  createSpace,
+  goToAPIKeyPage,
+  copyAPIKeyToClipBoard,
+  copiedAPIKeyValue,
+  deleteAPIKey
+} from 'web/actions/space';
 import { getNotificationMessageText } from 'web/actions/common';
 import spaceData from 'web/data/passiveNotification.json';
 import constants from 'data/constants.json';
 
 describe('Space API Key Tests', () => {
-    before(() => {
-        accountPage.open()
-        createAccount();
-        createSpace();
-        goToAPIKeyPage();
-    });
+  before(() => {
+    accountPage.open();
+    createAccount();
+    createSpace();
+    goToAPIKeyPage();
+  });
 
-    it('Copy --> verify key is copied', () => {
-        expect(verifyAPIKeyStatus(constants.APIKeyStatus.Active)).to.equal(true);
-        copyAPIKeyToClipBoard();
-        expect(getNotificationMessageText()).to.include(spaceData.copyNotificationMessage.text);
-        expect(copiedValue()).to.deep.equal(defaultAPIKey());
-    });
+  it('Copy --> verify key is copied', () => {
+    expect(verifyAPIKeyStatus(constants.APIKeyStatus.Active)).to.equal(true);
+    copyAPIKeyToClipBoard();
+    expect(getNotificationMessageText()).to.include(spaceData.copyNotificationMessage.text);
+    expect(copiedAPIKeyValue()).to.deep.equal(getAPIKey());
+  });
 
-    it('Revoke --> verify key is revoked', () => {
-        clickRevokeButton();
-        expect(verifyAPIKeyStatus(constants.APIKeyStatus.Revoked)).to.equal(true);
-    });
+  it('Revoke --> verify key is revoked', () => {
+    clickRevokeButton();
+    expect(verifyAPIKeyStatus(constants.APIKeyStatus.Revoked)).to.equal(true);
+  });
 
-    it('Undo --> verify key is re-activated', () => {
-        clickUndoButton();
-        expect(verifyAPIKeyStatus(constants.APIKeyStatus.Active)).to.equal(true);
-        expect(revokeButtonAppears()).to.equal(true);
-    });
+  it('Undo --> verify key is re-activated', () => {
+    clickUndoButton();
+    expect(verifyAPIKeyStatus(constants.APIKeyStatus.Active)).to.equal(true);
+    expect(revokeButtonAppears()).to.equal(true);
+  });
 
-    it('Delete --> verify key is deleted', () => {
-        deleteAPIKey();
-        expect(verifyAPIKeyStatus(constants.APIKeyStatus.PendingDelete)).to.equal(true);
-        expect(ifIconsEnabled()).to.equal(false);
-    });
+  it('Delete --> verify key is deleted', () => {
+    deleteAPIKey();
+    expect(verifyAPIKeyStatus(constants.APIKeyStatus.PendingDelete)).to.equal(true);
+    expect(ifIconsEnabled()).to.equal(false);
+  });
 });
