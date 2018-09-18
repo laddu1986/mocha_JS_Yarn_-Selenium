590c0d29668655ce3e6c6a2b86542f772537384c
abhidp <abhi358@gmail.com>
2018-09-18 13:20:07 +1000

    refactor

--- test/api/config/getEnv.js
+++ test/api/config/getEnv.js
@@ -20,68 +20,34 @@ function namespace() {
   });
 
   if (envArg == '' || envArg === undefined) {
-    identities = process.env.API_IDENTITIES_DEV
-    memberships = process.env.API_MEMBERSHIPS_DEV
-    organizations = process.env.API_ORGANIZATIONS_DEV
-    spaces = process.env.API_SPACES_DEV
-    token = process.env.API_TOKEN_DEV
-    invites = process.env.API_INVITES_DEV
-    keys = process.env.API_KEYS_DEV
-    metrics = process.env.API_METRICS_DEV
+    getEndPointsFor('DEV')
   }
   else {
     switch (envArg) {
       case 'qa': case 'QA': case 'Qa':
-        identities = process.env.API_IDENTITIES_QA
-        memberships = process.env.API_MEMBERSHIPS_QA
-        organizations = process.env.API_ORGANIZATIONS_QA
-        spaces = process.env.API_SPACES_QA
-        token = process.env.API_TOKEN_QA
-        invites = process.env.API_INVITES_QA
-        keys = process.env.API_KEYS_QA
-        metrics = process.env.API_METRICS_QA
+        getEndPointsFor('QA')
         break;
       case 'dev': case 'DEV': case 'Dev':
-        identities = process.env.API_IDENTITIES_DEV
-        memberships = process.env.API_MEMBERSHIPS_DEV
-        organizations = process.env.API_ORGANIZATIONS_DEV
-        spaces = process.env.API_SPACES_DEV
-        token = process.env.API_TOKEN_DEV
-        invites = process.env.API_INVITES_DEV
-        keys = process.env.API_KEYS_DEV
-        metrics = process.env.API_METRICS_DEV
+        getEndPointsFor('DEV')
         break;
       case 'prod': case 'PROD': case 'Prod':
-        identities = process.env.API_IDENTITIES_DEV
-        memberships = process.env.API_MEMBERSHIPS_DEV
-        organizations = process.env.API_ORGANIZATIONS_DEV
-        spaces = process.env.API_SPACES_DEV
-        token = process.env.API_TOKEN_DEV
-        invites = process.env.API_INVITES_DEV
-        keys = process.env.API_KEYS_DEV
-        metrics = process.env.API_METRICS_DEV
+        getEndPointsFor('PROD')
         break;
       default:
-        identities = process.env.API_IDENTITIES_DEV
-        memberships = process.env.API_MEMBERSHIPS_DEV
-        organizations = process.env.API_ORGANIZATIONS_DEV
-        spaces = process.env.API_SPACES_DEV
-        token = process.env.API_TOKEN_DEV
-        invites = process.env.API_INVITES_DEV
-        keys = process.env.API_KEYS_DEV
-        metrics = process.env.API_METRICS_DEV
+        getEndPointsFor('DEV')
     }
   }
+}
 
-  return envArg,
-    identities,
-    organizations,
-    memberships,
-    spaces,
-    token,
-    invites,
-    keys,
-    metrics;
+function getEndPointsFor(ENV) {
+  identities = process.env[`API_IDENTITIES_${ENV}`]
+  organizations = process.env[`API_ORGANIZATIONS_${ENV}`]
+  memberships = process.env[`API_MEMBERSHIPS_${ENV}`]
+  spaces = process.env[`API_SPACES_${ENV}`]
+  token = process.env[`API_TOKEN_${ENV}`]
+  invites = process.env[`API_INVITES_${ENV}`]
+  keys = process.env[`API_KEYS_${ENV}`]
+  metrics = process.env[`API_METRICS_${ENV}`]
 }
 
 namespace()
