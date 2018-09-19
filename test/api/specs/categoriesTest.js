cd45230eb94b87bf952c7dde378cec458aa7dfbf
Paul Sanchez <paul.sanchez@massive.co>
2018-09-19 14:24:32 +1000

    Delete Category

--- test/api/specs/categoriesTest.js
+++ test/api/specs/categoriesTest.js
@@ -3,7 +3,8 @@ import * as categories from 'api/actions/categories'
 import * as spaces from 'api/actions/spaces';
 import * as organization from 'api/actions/organization';
 import * as identity from 'api/actions/identity';
-var schema, createResponse, listResponse, renameResponse, moveResponse, deleteConfirm;
+
+var schema, createResponse, listResponse, renameResponse, moveConfirm, deleteConfirm;
 
 var schemaCategory = lib.joi.object().keys({
   id: lib.joi.number().integer().required(),
@@ -11,8 +12,8 @@ var schemaCategory = lib.joi.object().keys({
   isDefault: lib.joi.boolean().optional()
 });
 
-describe.only('Categories API', () => {
-  describe.only('createCategory()', () => {
+describe('Categories API', () => {
+  describe('createCategory()', () => {
     before('Initialise working Space', (done) => { 
       identity.postIdentity(lib.responseData.categories).then(() => { 
         return organization.postOrganization(lib.responseData.categories);
@@ -61,36 +62,39 @@ describe.only('Categories API', () => {
     });
   });
 
-  describe.only('moveCategory()', () => {
-    /*
-      TODO: 
-        Create a category
-        Fetch the list of all categories
-        Verify that the category has moved
-    */
+  describe('moveCategory()', () => {
     before('Send move request', (done) => {
       categories.createCategory(lib.responseData.categories).then(() => {
         return categories.moveCategory(categories.category);
       }).then(() => {
-        moveResponse = categories.listCategories(categories.category);
-        done()
+        moveConfirm = categories.listCategories(categories.category);
+        done();
       })
     });
 
     it('Moves the category', () => {
-      return moveResponse.then((response) => {
-        console.log(response);
+      return moveConfirm.then((response) => {
+       expect(response.categories[0].id > response.categories[1].id).to.be.true;
       })
     });
   });
 
-  describe.skip('deleteCategory()', () => {
-    before('Delete a category', (done) => {
-      categories.delateCategory(testCategory).then(() => {
+  describe('deleteCategory()', () => {
+    before('Send delete request', (done) => {
+      categories.deleteCategory(categories.category).then(() => {
         deleteConfirm = categories.listCategories(categories.category);
         done();
       })
-    })
+    });
     
+    it('Deletes the category', () => {
+      return deleteConfirm.then((response) => {
+        expect(response.categories).to.be.an('array');
+        expect(response.categories).to.have.lengthOf(2);
+        expect(response.categories[1]).to.have.property('isDefault', true);
+        expect(response.categories[0].id).to.not.equal(categories.category.id);
+        expect(response.categories[1].id).to.not.equal(categories.category.id);
+      });
+    });
   })
 });
