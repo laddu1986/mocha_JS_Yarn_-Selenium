e807156ed619032e98ca0d45b5ebd2e656d7f4a0
Balpreet Kaur <balpreet.kaur@massive.co>
2018-09-19 15:49:06 +1000

    commit3

--- test/api/actions/tribe.js
+++ test/api/actions/tribe.js
@@ -1,8 +1,8 @@
 
 import * as lib from '../../common';
 
-const PROTO_PATH = lib.path.resolve(process.env.PROTO_DIR + 'segmentService.proto');
-const client = lib.caller(process.env.LOCALHOST, PROTO_PATH, 'SegmentService');
+const PROTO_PATH = lib.path.resolve(process.env.TRIBE_PROTO_DIR + 'segmentService.proto');
+const client = lib.caller(process.env.TRIBE_HOST, PROTO_PATH, 'SegmentService');
 
 export function createTribe(responseObject) {
     const req = new client.Request('createSegment', { spaceContext: { orgId: responseObject.orgID, spaceId: responseObject.spaceID }, segment: { title: lib.randomString.generate(6) } }).withResponseStatus(true);
