9a14dd49ede479d824eb30658427c486b44ea126
Paul Sanchez <paul.sanchez@massive.co>
2018-09-20 15:57:36 +1000

    var names more unique, updated for env

--- test/api/actions/categories.js
+++ test/api/actions/categories.js
@@ -1,6 +1,6 @@
 import * as lib from '../../common';
 
-const PROTO_PATH = lib.path.resolve(process.env.PROTO_DIR + 'segmentService.proto');
+const PROTO_PATH = lib.path.resolve(process.env.TRIBE_PROTO_DIR + 'segmentService.proto');
 const client = lib.caller(process.env.TRIBE_HOST, PROTO_PATH, 'SegmentService');
 
 function createCategory(responseObject, updateFlag) {
@@ -13,8 +13,8 @@ function createCategory(responseObject, updateFlag) {
 
   return req.exec().then((response) => {
     if (updateFlag) { 
-      responseObject.label = label;
-      responseObject.id = response.response.id;
+      responseObject.tribeCategoryLabel = label;
+      responseObject.tribeCategoryID = response.response.id;
     }
     return response;
   });
@@ -33,14 +33,14 @@ function listCategories(responseObject) {
 function renameCategory(responseObject) {
   let newLabel = lib.randomString.generate(7);
   const req = new client.Request('renameCategory', {
-    categoryContext: { orgId: responseObject.orgID, spaceId: responseObject.spaceID, categoryId: responseObject.id },
+    categoryContext: { orgId: responseObject.orgID, spaceId: responseObject.spaceID, categoryId: responseObject.tribeCategoryID },
     label: newLabel
   }).withResponseStatus(true);
 
   return req.exec().then((response) => {
     if (response.status.code === 0) {
-      responseObject.oldLabel = responseObject.label;
-      responseObject.label = newLabel;
+      responseObject.tribeCategoryOldLabel = responseObject.tribeCategoryLabel;
+      responseObject.tribeCategoryLabel = newLabel;
     }
     return response;
   });
@@ -48,7 +48,7 @@ function renameCategory(responseObject) {
 
 function moveCategory(responseObject) {
   const req = new client.Request('moveCategory', {
-    categoryContext: { orgId: responseObject.orgID, spaceId: responseObject.spaceID, categoryId: responseObject.id },
+    categoryContext: { orgId: responseObject.orgID, spaceId: responseObject.spaceID, categoryId: responseObject.tribeCategoryID },
     index: { value: 10 }
   }).withResponseStatus(true);
 
@@ -59,7 +59,7 @@ function moveCategory(responseObject) {
 
 function deleteCategory(responseObject) {
   const req = new client.Request('deleteCategory', {
-    categoryContext: { orgId: responseObject.orgID, spaceId: responseObject.spaceID, categoryId: responseObject.id }
+    categoryContext: { orgId: responseObject.orgID, spaceId: responseObject.spaceID, categoryId: responseObject.tribeCategoryID }
   }).withResponseStatus(true);
 
   return req.exec().then((response) => {
