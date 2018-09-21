d1955e995f1de171a7b2208b4919fa516f042297
abhidp <abhi358@gmail.com>
2018-09-21 13:00:13 +1000

    getConfiguration

--- test/api/actions/tribeRules.js
+++ test/api/actions/tribeRules.js
@@ -6,24 +6,42 @@ const client = lib.caller(process.env.TRIBE_HOST, PROTO_PATH, 'SegmentRulesServi
 export function saveRule(responseObject) {
   const req = new client.Request('saveRule', {
     segmentContext: { orgId: responseObject.orgID, spaceId: responseObject.spaceID, segmentId: responseObject.tribeID },
-    rule: {
-      // audienceType: ,
-      // logicalType: ,
-      // repeatedFilter: 
-    }
+    rule: {}
   }).withResponseStatus(true);
+
+  return req.exec().then((response) => {
+    return response;
+  });
 }
 
-export function getRule() {
+export function getRule(responseObject) {
   //TODO: do this
+  const req = new client.Request('getRule', {
+    segmentContext: { orgId: responseObject.orgID, spaceId: responseObject.spaceID, segmentId: responseObject.tribeID }
+  }).withResponseStatus(true);
+
+  return req.exec().then((response) => {
+    responseObject.rule = response.response;
+    return response;
+  });
 }
 
-export function getConfiguration () {
+export function getConfiguration (responseObject) {
   //TODO: do this
+  const req = new client.Request('getConfiguration', {
+    orgId: responseObject.orgID,
+    spaceId: responseObject.spaceID
+  }).withResponseStatus(true);
+
+  return req.exec().then((response) => {
+    // Save our properties and operators
+    responseObject.configurations = response.response;
+    return response;
+  });
 }
 
 export function evaluateRule() {
-  //TODO: do this
+
 }
 
 export function evaluateRules() {
--- test/api/specs/tribeRulesTest.js
+++ test/api/specs/tribeRulesTest.js
@@ -5,25 +5,76 @@ import * as organization from 'api/actions/organization';
 import * as tribe from 'api/actions/tribe';
 import * as rules from 'api/actions/tribeRules'
 
+var configResponse, saveResponse, getResponse, schema;
+
 const rulesData = new Object();
 
-describe('Tribe Rules Service', () => {
-  describe('saveRule()', () => {
-    before('Create a tribe', (done) => {
+describe.only('Tribe Rules Service', () => {
+  describe('getConfiguration()', () => {
+    before('Set up the testing environment', (done) => {
       identity.postIdentity(rulesData).then(() => {
         return organization.postOrganization(rulesData);
       }).then(() => {
         return spaces.postSpaceByOrganizationId(rulesData);
       }).then(() => {
-        return tribe.createTribe(rulesData);
-      }).then((response) => {
-        saveResponse = rules.saveRule(rulesData);
+        configResponse = rules.getConfiguration(rulesData);
         done();
       })
     });
+    
+    it('Gets the possible configurations for the space', () => {
+      return configResponse.then((response) => {
+        var propertiesSchema, operatorsSchema;
+        propertiesSchema = lib.joi.object().keys({
+          id: lib.joi.string().uuid().required(),
+          label: lib.joi.string().required(),
+          type: lib.joi.number().required(),
+          allowedOperatorIds: lib.joi.array().items(lib.joi.string().uuid()).required(),
+          groupLabel: lib.joi.string().required()
+        });
 
-    it('Saves a Rule', () => {
-      //TODO:
+        operatorsSchema = lib.joi.object().keys({
+          id: lib.joi.string().uuid().required(),
+          label: lib.joi.string().required(),
+          operandType: lib.joi.number().optional(),
+          groupLabel: lib.joi.string().optional()
+        });
+
+        schema = lib.joi.object().keys({
+          configuration: lib.joi.object().keys({
+            properties: lib.joi.array().items(propertiesSchema),
+            operators: lib.joi.array().items(operatorsSchema)
+          })
+        });
+
+        lib.joi.assert(response.response, schema);
+      })
+    });
+  });
+
+  describe('saveRule()', () => {
+    before('Save the rule', (done) => {
+      tribe.createTribe(rulesData).then(() => {
+        saveResponse = rules.saveRule(rulesData);
+        done();
+      });
+    })
+
+    it('The rule is saved', () => {
+      return saveResponse.then((response) => {
+        expect(response.response.success).to.be.true;
+      });
+    });
+  });
+
+  describe('getRule()', () => {
+    before('Get the rule', (done) => {
+      getResponse = rules.getRule(ruleData);
+      done();
+    });
+
+    it('The rule is returned', () => {
+      //TODO: HERE CHECK AGAINST SAVED RULE
     });
   });
 });
