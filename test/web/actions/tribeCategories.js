9c9290a5ec802f7ff3d453d8421b9da724e2b45f
Paul Sanchez <paul.sanchez@massive.co>
2018-10-23 10:36:38 +1100

    pageObjectified

--- test/web/actions/tribeCategories.js
+++ test/web/actions/tribeCategories.js
@@ -1,6 +1,20 @@
 import '../../common';
 import tribePage from 'web/page_objects/tribePage';
 
+export function createCategory() {
+  tribePage.insertCategoryButton.click();
+}
+
+export function waitForCategoryOptions() {
+  browser.waitUntil(
+    () => {
+      return tribePage.categoryMoreButton.isVisible() && tribePage.categoryTitle.isVisible();
+    },
+    3000,
+    'Category took too long to create'
+  );
+}
+
 export function renameCategory(newTitle) {
   tribePage.categoryMoreButton.click();
   tribePage.categoryMoreRename.click();
@@ -8,9 +22,17 @@ export function renameCategory(newTitle) {
   return browser.keys('\uE007');
 }
 
+export function verifyRenamedTitle(title) {
+  return tribePage.categoryTitle.getAttribute('value') === title;
+}
+
 export function deleteCategory() {
   do {
     tribePage.categoryMoreButton.click();
   } while (!tribePage.categoryMoreDelete.isVisible());
   return tribePage.categoryMoreDelete.click();
+}
+
+export function verifyLastCategoryDeleted() {
+  return tribePage.categoryTitle.getAttribute('value') === '' && tribePage.tribeCards.value.length === 1;
 }
