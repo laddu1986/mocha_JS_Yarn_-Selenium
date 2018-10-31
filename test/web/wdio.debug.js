9ea4d05e61fc2a87ad12a82b8e667aab2e3b3a25
Paul Sanchez <paul.sanchez@massive.co>
2018-11-01 10:41:21 +1100

    split wdio config

---
+++ test/web/wdio.debug.js
@@ -0,0 +1,21 @@
+var defaults = require('./wdio.appcurator.js').config;
+var _ = require('lodash');
+var overrides = {
+  protocol: 'http',
+  host: 'localhost',
+  port: '4444',
+  path: '',
+  maxInstances: 1,
+  waitForTimeout: 9999999,
+  mochaOpts: {
+    ui: 'bdd',
+    reporter: 'spec',
+    compilers: ['js:babel-register'],
+    timeout: 9999999,
+    grep: process.env.npm_config_grep
+  },
+  debug: true,
+  execArgv: ['--inspect=127.0.0.1:5858']
+};
+
+exports.config = _.defaultsDeep(overrides, defaults);
