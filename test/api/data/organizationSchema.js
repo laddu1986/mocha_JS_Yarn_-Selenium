9f3256135216b3cc34f3f599049a71f5c9a52618
abhidp <abhi358@gmail.com>
2018-11-12 15:49:10 +1100

    organizations

--- test/api/data/organizationSchema.js
+++ test/api/data/organizationSchema.js
@@ -1,48 +1,59 @@
-import { joi, orgData, organizationsSchemaData } from '../common';
+import { joi } from '../common';
 import * as Constants from 'constants.json';
 import * as organization from 'actions/organization';
 
-export const createOrgSchema = joi.object().keys({
-  id: joi
-    .string()
-    .uuid()
-    .required(),
-  name: joi.valid(organizationsSchemaData.name).required(),
-  createdByAccountId: joi.valid(orgData.identityID).required(),
-  rowVersion: joi.date().required(),
-  createdTime: joi.date().required(),
-  modifiedTime: joi.valid(null).required(),
-  rowStatus: joi.valid(Constants.APIKeyStatus.Active).required()
-});
-export const getOrganizationByIdSchema = joi.object().keys({
-  id: joi.valid(orgData.orgID).required(),
-  name: joi.valid(organizationsSchemaData.name).required(),
-  createdByAccountId: joi.valid(orgData.identityID).required(),
-  rowVersion: joi.date().required(),
-  createdTime: joi.date().required(),
-  modifiedTime: joi.valid(null).required(),
-  rowStatus: joi.valid(Constants.APIKeyStatus.Active.replace(/\s/g, '')).required()
-});
-const objectSchema = joi.object().keys({
-  id: joi.valid(orgData.orgID).required(),
-  name: joi.valid(organizationsSchemaData.name).required(),
-  createdByAccountId: joi.valid(orgData.identityID).required(),
-  rowVersion: joi.date().required(),
-  createdTime: joi.date().required(),
-  modifiedTime: joi.valid(null).required(),
-  rowStatus: joi.valid(Constants.APIKeyStatus.Active).required()
-});
-export const postOrganizationsSchema = joi
-  .array()
-  .items(objectSchema)
-  .required();
+export function createOrgSchema(orgData) {
+  return joi.object().keys({
+    id: joi
+      .string()
+      .uuid()
+      .required(),
+    name: joi.valid(orgData.orgName).required(),
+    createdByAccountId: joi.valid(orgData.identityID).required(),
+    rowVersion: joi.date().required(),
+    createdTime: joi.date().required(),
+    modifiedTime: joi.valid(null).required(),
+    rowStatus: joi.valid(Constants.APIKeyStatus.Active).required()
+  });
+}
+export function getOrganizationByIdSchema(orgData) {
+  return joi.object().keys({
+    id: joi.valid(orgData.orgID).required(),
+    name: joi.valid(orgData.orgName).required(),
+    createdByAccountId: joi.valid(orgData.identityID).required(),
+    rowVersion: joi.date().required(),
+    createdTime: joi.date().required(),
+    modifiedTime: joi.valid(null).required(),
+    rowStatus: joi.valid(Constants.APIKeyStatus.Active.replace(/\s/g, '')).required()
+  });
+}
+function orgSchema(orgData) {
+  return joi.object().keys({
+    id: joi.valid(orgData.orgID).required(),
+    name: joi.valid(orgData.orgName).required(),
+    createdByAccountId: joi.valid(orgData.identityID).required(),
+    rowVersion: joi.date().required(),
+    createdTime: joi.date().required(),
+    modifiedTime: joi.valid(null).required(),
+    rowStatus: joi.valid(Constants.APIKeyStatus.Active).required()
+  });
+}
 
-export const putOrgSchema = joi.object().keys({
-  id: joi.valid(orgData.orgID).required(),
-  name: joi.valid(organization.newName).required(),
-  createdByAccountId: joi.valid(orgData.identityID).required(),
-  rowVersion: joi.date().required(),
-  createdTime: joi.date().required(),
-  modifiedTime: joi.date().required(),
-  rowStatus: joi.valid(Constants.APIKeyStatus.Active).required()
-});
+export function postOrganizationsSchema(orgData) {
+  return joi
+    .array()
+    .items(orgSchema(orgData))
+    .required();
+}
+
+export function putOrgSchema(orgData) {
+  return joi.object().keys({
+    id: joi.valid(orgData.orgID).required(),
+    name: joi.valid(organization.newName).required(),
+    createdByAccountId: joi.valid(orgData.identityID).required(),
+    rowVersion: joi.date().required(),
+    createdTime: joi.date().required(),
+    modifiedTime: joi.date().required(),
+    rowStatus: joi.valid(Constants.APIKeyStatus.Active).required()
+  });
+}
