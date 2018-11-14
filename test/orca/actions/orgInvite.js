bd59b264fc0e1fb0e99d53b960bc5988416a8281
abhidp <abhi358@gmail.com>
2018-11-14 12:09:28 +1100

    Orca: orgInvite Tests

---
+++ test/orca/actions/orgInvite.js
@@ -0,0 +1,72 @@
+import { randomString, post, orca } from '../common';
+
+export function createOrgInvite(responseData) {
+  var invitedEmail = `${randomString.generate(8)}@email.com`;
+  const data = {
+    query:
+      'mutation CreateOrgInvite($input: CreateOrgInviteInput!) { createOrgInvite(input: $input) {total invites {token expiryDate createdTime email status}}}',
+    variables: {
+      input: {
+        emails: [invitedEmail],
+        organizationId: responseData.orgID
+      }
+    }
+  };
+  const any = {
+    api: orca,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      cookie: `cid=${responseData.ccookie}; aid= ${responseData.token}; pid=${responseData.pcookie}`
+    },
+    data: data
+  };
+
+  return post(any).then(response => {
+    responseData.orgInviteToken = response.response.body.data.createOrgInvite.invites[0].token;
+    responseData.inviteEmail = invitedEmail;
+    return response;
+  });
+}
+
+export function getInviteTokenDetail(responseData) {
+  const data = {
+    query:
+      'query getInviteTokenDetail($token: ID!) { orgInviteTokenInfo(token: $token) {email organizationName hasAccount status organizationId}}',
+    variables: {
+      token: responseData.orgInviteToken
+    }
+  };
+  const any = {
+    api: orca,
+    data: data
+  };
+
+  return post(any).then(response => {
+    return response;
+  });
+}
+
+export function acceptOrgInvite(responseData) {
+  const data = {
+    query:
+      'mutation acceptOrgInvite($input: AcceptOrgInviteInput!) { acceptOrgInvite(input: $input) {organization{id name slug createdByAccountId rowVersion createdTime modifiedTime spaces{id name slug} members{total members{name email accountId organizationId organizationName role{name permissionLevel} currentUser }} invites{total invites{token email expiryDate}}rowStatus }}}',
+    variables: {
+      input: {
+        token: responseData.orgInviteToken
+      }
+    }
+  };
+  const any = {
+    api: orca,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      cookie: `cid=${responseData.ccookie}; aid= ${responseData.token}; pid=${responseData.pcookie}`
+    },
+    data: data
+  };
+  return post(any).then(response => {
+    return response;
+  });
+}
