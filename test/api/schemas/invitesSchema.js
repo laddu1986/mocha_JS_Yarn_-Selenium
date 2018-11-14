8f98e9d79187f49fdb421d450f013be375f5e5c6
Paul Sanchez <paul.sanchez@massive.co>
2018-11-14 16:32:31 +1100

    invites

--- test/api/schemas/invitesSchema.js
+++ test/api/schemas/invitesSchema.js
@@ -1,35 +1,42 @@
-import { joi, invitesSchemaData, inviteData } from '../common';
+import { joi } from '../common';
 import * as Constants from 'constants.json';
 
-const objectSchema = joi.object().keys({
-  token: joi
-    .string()
-    .uuid()
-    .required(),
-  email: joi.valid(invitesSchemaData.email).required(),
-  createdTime: joi.date().required(),
-  status: joi.valid(Constants.InviteStatus.Pending).required(),
-  expiryDate: joi.date().required()
-});
-export const getInviteSchema = joi.object().keys({
-  totalRows: joi.valid(1).required(),
-  results: joi
-    .array()
-    .items(objectSchema)
-    .required()
-});
+function inviteSchema(inviteData) {
+  return joi.object().keys({
+    token: joi
+      .string()
+      .uuid()
+      .required(),
+    email: joi.valid(inviteData.inviteEmail).required(),
+    createdTime: joi.date().required(),
+    status: joi.valid(Constants.InviteStatus.Pending).required(),
+    expiryDate: joi.date().required()
+  });
+}
 
-export const getInviteByTokenSchema = joi.object().keys({
-  email: joi.valid(invitesSchemaData.email).required(),
-  organizationId: joi
-    .string()
-    .uuid()
-    .valid(inviteData.orgID)
-    .required(),
-  organizationName: joi.valid(inviteData.orgName).required(),
-  hasAccount: joi
-    .boolean()
-    .valid(false)
-    .required(),
-  status: joi.valid(Constants.InviteStatus.Pending).required()
-});
+export function getInviteSchema(inviteData) {
+  return joi.object().keys({
+    totalRows: joi.valid(1).required(),
+    results: joi
+      .array()
+      .items(inviteSchema(inviteData))
+      .required()
+  });
+}
+
+export function getInviteByTokenSchema(inviteData) {
+  return joi.object().keys({
+    email: joi.valid(inviteData.inviteEmail).required(),
+    organizationId: joi
+      .string()
+      .uuid()
+      .valid(inviteData.orgID)
+      .required(),
+    organizationName: joi.valid(inviteData.orgName).required(),
+    hasAccount: joi
+      .boolean()
+      .valid(false)
+      .required(),
+    status: joi.valid(Constants.InviteStatus.Pending).required()
+  });
+}
