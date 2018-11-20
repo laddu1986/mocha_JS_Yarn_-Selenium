9b77c2e09754650c97d52d0ea37d437f29e10cb3
abhidp <abhi358@gmail.com>
2018-11-20 16:17:18 +1100

    adding operation name for the post requests

--- test/orca/actions/usersMetrics.js
+++ test/orca/actions/usersMetrics.js
@@ -12,6 +12,7 @@ export function getUserMetrics(responseData) {
   const data = {
     query:
       'query GetSpaceMetrics($input: SpaceMetricsInput!) {spaceMetrics(input: $input) {totalUsers totalRequests dailyNewUsers {date count} dailyActiveUsers {date count}}}',
+    operationName: GetSpaceMetrics, // eslint-disable-line
     variables: {
       input: {
         spaceId: responseData.spaceID,
@@ -39,6 +40,7 @@ export function getSpaceUsers(responseData) {
   const data = {
     query:
       'query getSpaceUsers($spaceId: ID!, $searchTerm: String, $limit: Int) { spaceUsers(spaceId: $spaceId, searchTerm: $searchTerm, limit: $limit) {users{id}}}',
+    operationName: getSpaceUsers, // eslint-disable-line
     variables: {
       limit: 5,
       searchTerm: 'a',
@@ -63,6 +65,7 @@ export function getSpaceUsersOverview(responseData) {
   const data = {
     query:
       'query getSpaceUsersStats($spaceId: ID!) { spaceUsersOverview(spaceId: $spaceId){total currentlyActive newlyJoined atRisk lost visitors totalDiff currentlyActiveDiff newlyJoinedDiff atRiskDiff lostDiff visitorsDiff currentlyActivePercent newlyJoinedPercent atRiskPercent lostPercent}}',
+    operationName: getSpaceUsersStats, // eslint-disable-line
     variables: {
       spaceId: responseData.spaceID
     }
@@ -85,6 +88,7 @@ export function getSpaceLabels(responseData) {
   const data = {
     query:
       'query getSpaceLabels($spaceId: ID!, $searchTerm: String!) { getSpaceLabels(spaceId: $spaceId, searchTerm: $searchTerm)}',
+    operationName: getSpaceLabels, // eslint-disable-line
     variables: {
       spaceId: responseData.spaceID,
       searchTerm: 'a'
@@ -109,6 +113,7 @@ export function getAccessTokens(responseData) {
     // should add token in response once https://app.clickup.com/301733/t/7m24r is fixed, currently it returns null
     query:
       'query AccessTokens($spaceId: ID!, $userId: ID!) { spaceUserAccessTokens(spaceId: $spaceId, userId: $userId) { tokens { name active created}}}',
+    operationName: AccessTokens, // eslint-disable-line
     variables: {
       spaceId: responseData.spaceID,
       userId: responseData.AccountID
