cc184dfb71d63c25057d18871ae233d71feed3d5
Paul Sanchez <paul.sanchez@massive.co>
2018-11-21 14:47:57 +1100

    spaces

--- test/api/specs/spaceNegTest.js
+++ test/api/specs/spaceNegTest.js
@@ -41,275 +41,216 @@ const spaceNegData = new Object();
 describe('Negative Tests --> Space Api', () => {
   describe('POST /organizations/{orgId}/spaces', () => {
     describe('400 Error Response: Mandatory fields validation', () => {
-      before(done => {
-        identity.postIdentity(spaceNegData).then(() => {
-          organization.postOrganization(spaceNegData).then(() => {
-            spaces.postSpaceByOrganizationId(spaceNegData).then(() => {
-              noNamePostResponse = post(data.noName(spaceNegData));
-              blankNameResponse = post(data.blankName(spaceNegData));
-              noShortUrlPostResponse = post(data.noShortUrl(spaceNegData));
-              blankShortUrlResponse = post(data.blankShortUrl(spaceNegData));
-              noAccountIdPostResponse = post(data.noAccountId(spaceNegData));
-              bigNameResponse = post(data.bigName(spaceNegData));
-              bigShortUrlResponse = post(data.bigShortUrl(spaceNegData));
-              done();
-            });
-          });
-        });
+      before(async () => {
+        await identity.postIdentity(spaceNegData);
+        await organization.postOrganization(spaceNegData);
+        await spaces.postSpaceByOrganizationId(spaceNegData);
+        noNamePostResponse = await post(data.noName(spaceNegData));
+        blankNameResponse = await post(data.blankName(spaceNegData));
+        noShortUrlPostResponse = await post(data.noShortUrl(spaceNegData));
+        blankShortUrlResponse = await post(data.blankShortUrl(spaceNegData));
+        noAccountIdPostResponse = await post(data.noAccountId(spaceNegData));
+        bigNameResponse = await post(data.bigName(spaceNegData));
+        bigShortUrlResponse = await post(data.bigShortUrl(spaceNegData));
       });
       it('Name field is required', () => {
-        return noNamePostResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.noName(spaceNegData).expected);
-        });
+        expect(noNamePostResponse).to.have.status(400);
+        expect(noNamePostResponse.body.validationErrors.name).to.equal(data.noName(spaceNegData).expected);
       });
       it('ShortUrl field is required', () => {
-        return noShortUrlPostResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.shortUrl).to.equal(data.noShortUrl(spaceNegData).expected);
-        });
+        expect(noShortUrlPostResponse).to.have.status(400);
+        expect(noShortUrlPostResponse.body.validationErrors.shortUrl).to.equal(data.noShortUrl(spaceNegData).expected);
       });
       it('Name field cannot be blank', () => {
-        return blankNameResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.blankName(spaceNegData).expected);
-        });
+        expect(blankNameResponse).to.have.status(400);
+        expect(blankNameResponse.body.validationErrors.name).to.equal(data.blankName(spaceNegData).expected);
       });
       it('ShortUrl field cannot be blank', () => {
-        return blankShortUrlResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.shortUrl).to.equal(data.blankShortUrl(spaceNegData).expected);
-        });
+        expect(blankShortUrlResponse).to.have.status(400);
+        expect(blankShortUrlResponse.body.validationErrors.shortUrl).to.equal(
+          data.blankShortUrl(spaceNegData).expected
+        );
       });
       it('Name field cannot be >75 characters', () => {
-        return bigNameResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.bigName(spaceNegData).expected);
-        });
+        expect(bigNameResponse).to.have.status(400);
+        expect(bigNameResponse.body.validationErrors.name).to.equal(data.bigName(spaceNegData).expected);
       });
       it('ShortUrl field cannot be >20 characters', () => {
-        return bigShortUrlResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.shortUrl).to.equal(data.bigShortUrl(spaceNegData).expected);
-        });
+        expect(bigShortUrlResponse).to.have.status(400);
+        expect(bigShortUrlResponse.body.validationErrors.shortUrl).to.equal(data.bigShortUrl(spaceNegData).expected);
       });
       it('AccountID field is required', () => {
-        return noAccountIdPostResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.createdByAccountId).to.equal(data.noAccountId(spaceNegData).expected);
-        });
+        expect(noAccountIdPostResponse).to.have.status(400);
+        expect(noAccountIdPostResponse.body.validationErrors.createdByAccountId).to.equal(
+          data.noAccountId(spaceNegData).expected
+        );
       });
     });
   });
   describe('PUT /organizations/{orgId}/spaces', () => {
     describe('400 Error Response: Mandatory fields validation', () => {
-      before(done => {
-        noNamePutResponse = put(data.noNamePut(spaceNegData));
-        blankNamePutResponse = put(data.blankNamePut(spaceNegData));
-        bigNamePutResponse = put(data.bigNamePut(spaceNegData));
-        noShortUrlPutResponse = put(data.noShortUrlPut(spaceNegData));
-        blankShortUrlPutResponse = put(data.blankShortUrlPut(spaceNegData));
-        bigShortUrlPutResponse = put(data.bigShortUrlPut(spaceNegData));
-        noIdPutResponse = put(data.noIdPut(spaceNegData));
-        done();
+      before(async () => {
+        noNamePutResponse = await put(data.noNamePut(spaceNegData));
+        blankNamePutResponse = await put(data.blankNamePut(spaceNegData));
+        bigNamePutResponse = await put(data.bigNamePut(spaceNegData));
+        noShortUrlPutResponse = await put(data.noShortUrlPut(spaceNegData));
+        blankShortUrlPutResponse = await put(data.blankShortUrlPut(spaceNegData));
+        bigShortUrlPutResponse = await put(data.bigShortUrlPut(spaceNegData));
+        noIdPutResponse = await put(data.noIdPut(spaceNegData));
       });
       it('Name field is required', () => {
-        return noNamePutResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.noNamePut(spaceNegData).expected);
-        });
+        expect(noNamePutResponse).to.have.status(400);
+        expect(noNamePutResponse.body.validationErrors.name).to.equal(data.noNamePut(spaceNegData).expected);
       });
       xit('Name cannot be >75 characters', () => {
         // to be enabled when AF-167 is resolved
-        return bigNamePutResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.bigNamePut(spaceNegData).expected);
-        });
+        expect(bigNamePutResponse).to.have.status(400);
+        expect(bigNamePutResponse.body.validationErrors.name).to.equal(data.bigNamePut(spaceNegData).expected);
       });
       xit('Name cannot be blank', () => {
         // to be enabled when AF-167 is resolved
-        return blankNamePutResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.blankNamePut(spaceNegData).expected);
-        });
+        expect(blankNamePutResponse).to.have.status(400);
+        expect(blankNamePutResponse.body.validationErrors.name).to.equal(data.blankNamePut(spaceNegData).expected);
       });
       it('ShortUrl field is required', () => {
-        return noShortUrlPutResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.shortUrl).to.equal(data.noShortUrlPut(spaceNegData).expected);
-        });
+        expect(noShortUrlPutResponse).to.have.status(400);
+        expect(noShortUrlPutResponse.body.validationErrors.shortUrl).to.equal(
+          data.noShortUrlPut(spaceNegData).expected
+        );
       });
       it('ShortUrl field cannot be empty', () => {
-        return blankShortUrlPutResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.shortUrl).to.equal(data.blankShortUrlPut(spaceNegData).expected);
-        });
+        expect(blankShortUrlPutResponse).to.have.status(400);
+        expect(blankShortUrlPutResponse.body.validationErrors.shortUrl).to.equal(
+          data.blankShortUrlPut(spaceNegData).expected
+        );
       });
       it('ShortUrl cannot be >20 characters', () => {
-        return bigShortUrlPutResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.shortUrl).to.equal(data.bigShortUrlPut(spaceNegData).expected);
-        });
+        expect(bigShortUrlPutResponse).to.have.status(400);
+        expect(bigShortUrlPutResponse.body.validationErrors.shortUrl).to.equal(
+          data.bigShortUrlPut(spaceNegData).expected
+        );
       });
       it('Id field is required', () => {
-        return noIdPutResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.id).to.equal(data.noIdPut(spaceNegData).expected);
-        });
+        expect(noIdPutResponse).to.have.status(400);
+        expect(noIdPutResponse.body.validationErrors.id).to.equal(data.noIdPut(spaceNegData).expected);
       });
     });
     describe('409 Error Response: Conflict', () => {
-      before(done => {
-        noRowVersionPutResponse = put(data.noRowVersionPut(spaceNegData));
-        done();
+      before(async () => {
+        noRowVersionPutResponse = await put(data.noRowVersionPut(spaceNegData));
       });
       it('RowVersion is not provided', () => {
-        return noRowVersionPutResponse.then(response => {
-          expect(response).to.have.status(409);
-        });
+        expect(noRowVersionPutResponse).to.have.status(409);
       });
     });
     describe('404 Error Response: Not Found', () => {
-      before(done => {
-        incorrectOrgIDResponse = put(data.incorrectOrgIDPut(spaceNegData));
-        incorrectSpaceIDResponse = put(data.incorrectSpaceIDPut(spaceNegData));
-        done();
+      before(async () => {
+        incorrectOrgIDResponse = await put(data.incorrectOrgIDPut(spaceNegData));
+        incorrectSpaceIDResponse = await put(data.incorrectSpaceIDPut(spaceNegData));
       });
       it('Space Id is not existing', () => {
-        return incorrectSpaceIDResponse.then(response => {
-          expect(response).to.have.status(404);
-        });
+        expect(incorrectSpaceIDResponse).to.have.status(404);
       });
       it('Org Id is not existing', () => {
-        return incorrectOrgIDResponse.then(response => {
-          expect(response).to.have.status(404);
-        });
+        expect(incorrectOrgIDResponse).to.have.status(404);
       });
     });
   });
   describe('GET /organizations/{orgId}/spaces/{spaceId}', () => {
     describe('404 Error Response: Not Found', () => {
-      before(done => {
-        incorrectOrgIDGetResponse = get(data.incorrectOrgIDGet(spaceNegData));
-        incorrectSpaceIDGetResponse = get(data.incorrectSpaceIDGet(spaceNegData));
-        done();
+      before(async () => {
+        incorrectOrgIDGetResponse = await get(data.incorrectOrgIDGet(spaceNegData));
+        incorrectSpaceIDGetResponse = await get(data.incorrectSpaceIDGet(spaceNegData));
       });
       it('OrgId is not existing', () => {
-        return incorrectOrgIDGetResponse.then(response => {
-          expect(response).to.have.status(404);
-        });
+        expect(incorrectOrgIDGetResponse).to.have.status(404);
       });
       it('SpaceId is not existing', () => {
-        return incorrectSpaceIDGetResponse.then(response => {
-          expect(response).to.have.status(404);
-        });
+        expect(incorrectSpaceIDGetResponse).to.have.status(404);
       });
     });
   });
   describe('PATCH /organizations/{orgId}/spaces/{spaceId}', () => {
     describe('409 Error Response: Conflict', () => {
-      before(done => {
-        incorrectRowVersionPatchResponse = patch(data.incorrectRowVersionPatch(spaceNegData));
-        done();
+      before(async () => {
+        incorrectRowVersionPatchResponse = await patch(data.incorrectRowVersionPatch(spaceNegData));
       });
       it('RowVersion is not existing', () => {
-        return incorrectRowVersionPatchResponse.then(response => {
-          expect(response).to.have.status(409);
-        });
+        expect(incorrectRowVersionPatchResponse).to.have.status(409);
       });
     });
     describe('404 Error Response: Not Found', () => {
-      before(done => {
-        incorrectOrgIDPatchResponse = patch(data.incorrectOrgIDPatch(spaceNegData));
-        incorrectSpaceIDPatchResponse = patch(data.incorrectSpaceIDPatch(spaceNegData));
-        done();
+      before(async () => {
+        incorrectOrgIDPatchResponse = await patch(data.incorrectOrgIDPatch(spaceNegData));
+        incorrectSpaceIDPatchResponse = await patch(data.incorrectSpaceIDPatch(spaceNegData));
       });
       it('OrgId is not existing', () => {
-        return incorrectOrgIDPatchResponse.then(response => {
-          expect(response).to.have.status(404);
-        });
+        expect(incorrectOrgIDPatchResponse).to.have.status(404);
       });
       it('SpaceId is not existing', () => {
-        return incorrectSpaceIDPatchResponse.then(response => {
-          expect(response).to.have.status(404);
-        });
+        expect(incorrectSpaceIDPatchResponse).to.have.status(404);
       });
     });
     describe('400 Error Response: Mandatory fields validation', () => {
-      before(done => {
-        blankNamePatchResponse = patch(data.blankNamePatch(spaceNegData));
-        noNamePatchResponse = patch(data.noNamePatch(spaceNegData));
-        bigNamePatchResponse = patch(data.bigNamePatch(spaceNegData));
-        blankShortUrlPatchResponse = patch(data.blankShortUrlPatch(spaceNegData));
-        noShortUrlPatchResponse = patch(data.noShortUrlPatch(spaceNegData));
-        bigShortUrlPatchResponse = patch(data.bigShortUrlPatch(spaceNegData));
-        done();
+      before(async () => {
+        blankNamePatchResponse = await patch(data.blankNamePatch(spaceNegData));
+        noNamePatchResponse = await patch(data.noNamePatch(spaceNegData));
+        bigNamePatchResponse = await patch(data.bigNamePatch(spaceNegData));
+        blankShortUrlPatchResponse = await patch(data.blankShortUrlPatch(spaceNegData));
+        noShortUrlPatchResponse = await patch(data.noShortUrlPatch(spaceNegData));
+        bigShortUrlPatchResponse = await patch(data.bigShortUrlPatch(spaceNegData));
       });
       it('Name cannot be blank', () => {
-        return blankNamePatchResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.blankNamePatch(spaceNegData).expected);
-        });
+        expect(blankNamePatchResponse).to.have.status(400);
+        expect(blankNamePatchResponse.body.validationErrors.name).to.equal(data.blankNamePatch(spaceNegData).expected);
       });
       it('Name is required', () => {
-        return noNamePatchResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.noNamePatch(spaceNegData).expected);
-        });
+        expect(noNamePatchResponse).to.have.status(400);
+        expect(noNamePatchResponse.body.validationErrors.name).to.equal(data.noNamePatch(spaceNegData).expected);
       });
       it('Name cannot be >75 characters', () => {
-        return bigNamePatchResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.bigNamePatch(spaceNegData).expected);
-        });
+        expect(bigNamePatchResponse).to.have.status(400);
+        expect(bigNamePatchResponse.body.validationErrors.name).to.equal(data.bigNamePatch(spaceNegData).expected);
       });
       it('ShortUrl cannot be blank', () => {
-        return blankShortUrlPatchResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.shortUrl).to.equal(data.blankShortUrlPatch(spaceNegData).expected);
-        });
+        expect(blankShortUrlPatchResponse).to.have.status(400);
+        expect(blankShortUrlPatchResponse.body.validationErrors.shortUrl).to.equal(
+          data.blankShortUrlPatch(spaceNegData).expected
+        );
       });
       it('ShortUrl is required', () => {
-        return noShortUrlPatchResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.shortUrl).to.equal(data.noShortUrlPatch(spaceNegData).expected);
-        });
+        expect(noShortUrlPatchResponse).to.have.status(400);
+        expect(noShortUrlPatchResponse.body.validationErrors.shortUrl).to.equal(
+          data.noShortUrlPatch(spaceNegData).expected
+        );
       });
       it('ShortUrl cannot be >20 characters', () => {
-        return bigShortUrlPatchResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.shortUrl).to.equal(data.bigShortUrlPatch(spaceNegData).expected);
-        });
+        expect(bigShortUrlPatchResponse).to.have.status(400);
+        expect(bigShortUrlPatchResponse.body.validationErrors.shortUrl).to.equal(
+          data.bigShortUrlPatch(spaceNegData).expected
+        );
       });
     });
   });
   describe('DELETE /organizations/{orgId}/spaces/{spaceId}', () => {
     describe('404 Error Response: Not Found', () => {
-      before(done => {
-        incorrectSpaceIDDeleteResponse = del(data.incorrectSpaceIDDelete(spaceNegData));
-        incorrectOrgIDDeleteResponse = del(data.incorrectOrgIDDelete(spaceNegData));
-        done();
+      before(async () => {
+        incorrectSpaceIDDeleteResponse = await del(data.incorrectSpaceIDDelete(spaceNegData));
+        incorrectOrgIDDeleteResponse = await del(data.incorrectOrgIDDelete(spaceNegData));
       });
       it('Space ID is not existing', () => {
-        return incorrectSpaceIDDeleteResponse.then(response => {
-          expect(response).to.have.status(404);
-        });
+        expect(incorrectSpaceIDDeleteResponse).to.have.status(404);
       });
       it('Org ID is not existing', () => {
-        return incorrectOrgIDDeleteResponse.then(response => {
-          expect(response).to.have.status(404);
-        });
+        expect(incorrectOrgIDDeleteResponse).to.have.status(404);
       });
     });
     describe('409 Error Response: Conflict', () => {
-      before(done => {
-        noRowVersionDeleteResponse = del(data.noRowVersionDelete(spaceNegData));
-        done();
+      before(async () => {
+        noRowVersionDeleteResponse = await del(data.noRowVersionDelete(spaceNegData));
       });
       it('RowVersion is not existing', () => {
-        return noRowVersionDeleteResponse.then(response => {
-          expect(response).to.have.status(409);
-        });
+        expect(noRowVersionDeleteResponse).to.have.status(409);
       });
     });
   });
