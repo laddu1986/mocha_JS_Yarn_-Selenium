f2dd5cdf2465809471c386a98f502883f1949d66
Paul Sanchez <paul.sanchez@massive.co>
2018-11-21 14:22:35 +1100

    organization categories

--- test/api/specs/organizationNegTest.js
+++ test/api/specs/organizationNegTest.js
@@ -18,89 +18,65 @@ const orgNegData = new Object();
 describe('Negative Tests --> Organizations Api', () => {
   describe('POST /organizations', () => {
     describe('400 Error Response: Mandatory fields validation', () => {
-      before(done => {
-        identity.postIdentity(orgNegData).then(() => {
-          organization.postOrganization(orgNegData).then(() => {
-            noNamePostResponse = post(data.noName(orgNegData));
-            noIDPostResponse = post(data.blankAccountId);
-            done();
-          });
-        });
+      before(async () => {
+        await identity.postIdentity(orgNegData);
+        await organization.postOrganization(orgNegData);
+        noNamePostResponse = await post(data.noName(orgNegData));
+        noIDPostResponse = await post(data.blankAccountId);
       });
       it('Name field is required', () => {
-        return noNamePostResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.noName(orgNegData).expected);
-        });
+        expect(noNamePostResponse).to.have.status(400);
+        expect(noNamePostResponse.body.validationErrors.name).to.equal(data.noName(orgNegData).expected);
       });
       xit('CreatedByAccountId field is required', () => {
         // to be enabled when ACF-212 is fixed
-        return noIDPostResponse.then(response => {
-          expect(response).to.have.status(400);
-          expect(response.body.validationErrors.name).to.equal(data.blankAccountId.expected);
-        });
+        expect(noIDPostResponse).to.have.status(400);
+        expect(noIDPostResponse.body.validationErrors.name).to.equal(data.blankAccountId.expected);
       });
     });
   });
   describe('PUT /organizations', () => {
-    before(done => {
-      noRowVersionPutResponse = put(data.blankRowVersion(orgNegData));
-      blankNamePutResponse = put(data.blankName(orgNegData));
-      blankIDPutResponse = put(data.blankID(orgNegData));
-      incorrectIDPutResponse = put(data.incorrectOrgIDPut(orgNegData));
-      done();
+    before(async () => {
+      noRowVersionPutResponse = await put(data.blankRowVersion(orgNegData));
+      blankNamePutResponse = await put(data.blankName(orgNegData));
+      blankIDPutResponse = await put(data.blankID(orgNegData));
+      incorrectIDPutResponse = await put(data.incorrectOrgIDPut(orgNegData));
     });
     it('409 Error Response: RowVersion Conflict', () => {
-      return noRowVersionPutResponse.then(response => {
-        expect(response).to.have.status(409);
-        expect(response.body).to.equal(data.blankRowVersion(orgNegData).expected);
-      });
+      expect(noRowVersionPutResponse).to.have.status(409);
+      expect(noRowVersionPutResponse.body).to.equal(data.blankRowVersion(orgNegData).expected);
     });
     it('400 Error Response: Blank Name', () => {
-      return blankNamePutResponse.then(response => {
-        expect(response).to.have.status(400);
-        expect(response.body.validationErrors.name).to.equal(data.blankName(orgNegData).expected);
-      });
+      expect(blankNamePutResponse).to.have.status(400);
+      expect(blankNamePutResponse.body.validationErrors.name).to.equal(data.blankName(orgNegData).expected);
     });
     it('400 Error Response: Blank ID', () => {
-      return blankIDPutResponse.then(response => {
-        expect(response).to.have.status(400);
-        expect(response.body.validationErrors.id).to.equal(data.blankID(orgNegData).expected);
-      });
+      expect(blankIDPutResponse).to.have.status(400);
+      expect(blankIDPutResponse.body.validationErrors.id).to.equal(data.blankID(orgNegData).expected);
     });
     it('404 Error Response: Non Existing Org ID', () => {
-      return incorrectIDPutResponse.then(response => {
-        expect(response).to.have.status(404);
-      });
+      expect(incorrectIDPutResponse).to.have.status(404);
     });
   });
   describe('GET /organizations/{id}', () => {
-    before(done => {
-      getResponse = get(data.incorrrectOrgIDGet(orgNegData));
-      done();
+    before(async () => {
+      getResponse = await get(data.incorrrectOrgIDGet(orgNegData));
     });
     it('404 Error Response: Non Existing Org ID', () => {
-      return getResponse.then(response => {
-        expect(response).to.have.status(404);
-      });
+      expect(getResponse).to.have.status(404);
     });
   });
   describe('DELETE /organizations/{id}', () => {
-    before(done => {
-      incorrectOrgIDDeleteResponse = del(data.incorrectOrgIDDelete(orgNegData));
-      blankOrgIdDeleteResponse = del(data.blankOrgIdDelete);
-      done();
+    before(async () => {
+      incorrectOrgIDDeleteResponse = await del(data.incorrectOrgIDDelete(orgNegData));
+      blankOrgIdDeleteResponse = await del(data.blankOrgIdDelete);
     });
     it('409 Error Response: Non Existing Org ID', () => {
-      return incorrectOrgIDDeleteResponse.then(response => {
-        expect(response).to.have.status(409);
-        expect(response.body).to.equal(data.incorrectOrgIDDelete(orgNegData).expected);
-      });
+      expect(incorrectOrgIDDeleteResponse).to.have.status(409);
+      expect(incorrectOrgIDDeleteResponse.body).to.equal(data.incorrectOrgIDDelete(orgNegData).expected);
     });
     it('404 Error Response: Non Existing Org ID', () => {
-      return blankOrgIdDeleteResponse.then(response => {
-        expect(response).to.have.status(404);
-      });
+      expect(blankOrgIdDeleteResponse).to.have.status(404);
     });
   });
 });
