b8a5e10ac7f1fcc69d136663a3de26c154e84922
abhidp <abhi358@gmail.com>
2018-11-28 13:53:56 +1100

    userService

--- test/api/actions/user.js
+++ test/api/actions/user.js
@@ -1,14 +1,93 @@
-import { path, caller } from '../common';
+import { path, caller, randomString } from '../common';
 
 const PROTO_PATH = path.resolve(process.env.USER_PROTO_DIR + 'spaceUserService.proto');
 const client = caller(process.env.USER_HOST, PROTO_PATH, 'spaceUserService');
 
-export function listSpaceUsers(reponseObject) {
-  const req = new client.Request('listSpaceUsers', {
-    spaceId: '82e166fa3-a1ce-40f1-9f4b-865d00164587',
-    keyword: 'a',
-    limit: 3
+export function identifySpaceUser(userData) {
+  userData.userInternalID = randomString.generate(16);
+
+  const req = new client.Request('identifySpaceUser', {
+    externalId: 'AutomatedTest',
+    organizationId: userData.orgID,
+    spaceId: userData.spaceID,
+    internalId: userData.userInternalID,
+    email: userData.identityEmail,
+    lastIpAddress: '1.1.1.1',
+    audienceType: 'USER',
+    lastDeviceType: 'AutomatedUser',
+    displayName: 'AutomatedUser',
+    lastLocation: {
+      country: 'AU',
+      countryCode: 'AU',
+      city: 'AUTOCITY'
+    }
   }).withResponseStatus(true);
 
-  return req.exec()
+  return req.exec();
+}
+
+export function listSpaceUsers(userData) {
+  const req = new client.Request('listSpaceUsers', {
+    spaceId: userData.spaceID,
+    keyword: 'Auto',
+    limit: 20
+  }).withResponseStatus(true);
+
+  return req.exec();
+}
+
+export function getSpaceUserStatistics(userData) {
+  const req = new client.Request('getSpaceUserStatistics', {
+    spaceId: userData.spaceID
+  }).withResponseStatus(true);
+
+  return req.exec();
+}
+
+export function getSpaceUserDetails(userData) {
+  const req = new client.Request('getSpaceUserDetails', {
+    spaceId: userData.spaceID,
+    userId: userData.userInternalID
+  }).withResponseStatus(true);
+
+  return req.exec();
+}
+
+export function addSpaceUserLabels(userData, labels) {
+  userData.labels = labels;
+  const req = new client.Request('addSpaceUserLabels', {
+    userIds: [userData.userInternalID],
+    spaceId: userData.spaceID,
+    labels: labels
+  }).withResponseStatus(true);
+  return req.exec();
+}
+
+export function removeSpaceUserLabels(userData) {
+  const req = new client.Request('removeSpaceUserLabels', {
+    userIds: [userData.userInternalID],
+    spaceId: userData.spaceID,
+    labels: userData.labels
+  }).withResponseStatus(true);
+
+  return req.exec();
+}
+
+export function searchLabels(userData) {
+  const req = new client.Request('searchSpaceUserLabels', {
+    spaceId: userData.spaceID,
+    limit: 5,
+    keyword: userData.labels
+  }).withResponseStatus(true);
+
+  return req.exec();
+}
+
+export function deleteSpaceUsers(userData) {
+  const req = new client.Request('deleteSpaceUsers', {
+    spaceId: userData.spaceID,
+    userIds: [userData.internalId]
+  }).withResponseStatus(true);
+
+  return req.exec();
 }
