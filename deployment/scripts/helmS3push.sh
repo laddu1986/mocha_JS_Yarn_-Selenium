00a7ea51335ccffad27489afdad06f7a7686e976
Abhi Daspatnaik <abhijeet.daspatnaik@massive.co>
2018-12-09 15:17:08 +1100

    helmchart

--- deployment/scripts/helmS3push.sh
+++ deployment/scripts/helmS3push.sh
@@ -1,5 +1,5 @@
 helm init --client-only
-helm package --version 0.1.0 test/web/helmchart
+helm package --version 0.1.0 deployment/helmchart
 helm s3 init s3://$S3_BUCKET_NAME/helmchart
 helm repo add helmchart s3://$S3_BUCKET_NAME/helmchart
 helm s3 push ./helmchart-0.1.0.tgz helmchart
