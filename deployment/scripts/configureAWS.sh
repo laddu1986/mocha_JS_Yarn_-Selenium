773ea4b4a8e18dbd132a92d09411cc615f3b8a8d
abhidp <abhi358@gmail.com>
2018-12-09 14:36:19 +1100

    aws

---
+++ deployment/scripts/configureAWS.sh
@@ -0,0 +1,5 @@
+aws configure set AWS_ACCESS_KEY_ID AKIAIE7TXRZ3BAQTKJRQ
+aws configure set AWS_SECRET_ACCESS_KEY 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
+aws configure set AWS_DEFAULT_REGION ap-southeast-2
+aws configure set AWS_REGION ap-southeast-2
+aws configure set AWS_DEFAULT_OUTPUT text
