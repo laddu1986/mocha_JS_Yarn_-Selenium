f052378e12236d1e6518011594d83933be733a1d
abhidp <abhi358@gmail.com>
2018-12-10 21:59:56 +1100

    awshelm

--- deployment/scripts/AWSconfig.sh
+++ deployment/scripts/AWSconfig.sh
@@ -1,3 +1,10 @@
+#Install aws-cli
+apk add --no-cache python \
+    && apk add --no-cache --virtual .awscli-build-deps py-pip \
+    && pip install awscli \
+    && apk del --purge .awscli-build-deps
+
+#configure credentials
 aws configure set AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_ID
 aws configure set AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
 aws configure set AWS_DEFAULT_REGION $AWS_DEFAULT_REGION
--- deployment/scripts/helmS3push.sh
+++ deployment/scripts/helmS3push.sh
@@ -2,7 +2,7 @@ helm init --client-only
 helm package --version 0.1.0 deployment/helmchart
 # Create index.yaml
 helm repo index --url https://$S3_BUCKET_NAME/helmchart --home . .
-helm repo add helmchart s3://$S3_BUCKET_NAME/helmchart
+# helm repo add helmchart s3://$S3_BUCKET_NAME/helmchart
 # helm s3 push --force ./helmchart-0.1.0.tgz helmchart
 echo "Syncing index.yaml and packaged charts ..."
 aws s3 sync . s3://$S3_BUCKET_NAME/helmchart --exclude '*' --include "*.tgz" --include "index.yaml"
