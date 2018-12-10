bc47063306e8f62883f06922fe0ff20bc3f9702a
abhidp <abhi358@gmail.com>
2018-12-11 00:35:36 +1100

    publicread

--- deployment/scripts/helm-s3-push.sh
+++ deployment/scripts/helm-s3-push.sh
@@ -7,4 +7,4 @@ echo -e '\nCreating index.yaml file ...'
 helm repo index --url https://s3-${AWS_REGION}.amazonaws.com/${S3_BUCKET_NAME}/helmchart --home . .
 
 echo -e '\nSyncing index.yaml and packaged charts to S3...'
-aws s3 sync . s3://${S3_BUCKET_NAME}/helmchart --exclude '*' --include "*.tgz" --include "index.yaml"
+aws s3 sync . s3://${S3_BUCKET_NAME}/helmchart --exclude '*' --include "*.tgz" --include "index.yaml" --acl public-read
