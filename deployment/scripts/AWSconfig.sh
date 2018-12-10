0d7d97a4703c0aa5cdeb21bf4ab8df2835b95982
Abhi Daspatnaik <abhijeet.daspatnaik@massive.co>
2018-12-10 22:51:45 +1100

    indexyaml

--- deployment/scripts/AWSconfig.sh
+++ deployment/scripts/AWSconfig.sh
@@ -1,10 +1,10 @@
-#Install aws-cli
+echo '\nInstalling aws-cli ...'
 apk add --no-cache python \
     && apk add --no-cache --virtual .awscli-build-deps py-pip \
     && pip install awscli \
     && apk del --purge .awscli-build-deps
 
-#configure credentials
+echo '\nConfiguring AWS credentials'
 aws configure set AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_ID
 aws configure set AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
 aws configure set AWS_DEFAULT_REGION $AWS_DEFAULT_REGION
