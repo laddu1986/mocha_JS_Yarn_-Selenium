6bab731234a65150a8b2e5455d556ac8c42de020
Abhi Daspatnaik <abhijeet.daspatnaik@massive.co>
2018-12-10 22:55:28 +1100

    indexyaml

--- deployment/scripts/AWSconfig.sh
+++ deployment/scripts/AWSconfig.sh
@@ -1,10 +1,10 @@
-echo '\nInstalling aws-cli ...'
+echo -e '\nInstalling aws-cli ...'
 apk add --no-cache python \
     && apk add --no-cache --virtual .awscli-build-deps py-pip \
     && pip install awscli \
     && apk del --purge .awscli-build-deps
 
-echo '\nConfiguring AWS credentials'
+echo -e '\nConfiguring AWS credentials'
 aws configure set AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_ID
 aws configure set AWS_SECRET_ACCESS_KEY $AWS_SECRET_ACCESS_KEY
 aws configure set AWS_DEFAULT_REGION $AWS_DEFAULT_REGION
--- deployment/scripts/helmS3push.sh
+++ deployment/scripts/helmS3push.sh
@@ -1,10 +1,10 @@
 helm init --client-only
 
-echo '\nPackaging Helm chart ...'
+echo -e '\nPackaging Helm chart ...'
 helm package --version 0.1.0 deployment/helmchart
 
-echo '\nCreating index.yaml file ...'
+echo -e '\nCreating index.yaml file ...'
 helm repo index --url https://$S3_BUCKET_NAME/helmchart --home . .
 
-echo '\nSyncing index.yaml and packaged charts to S3...'
+echo -e '\nSyncing index.yaml and packaged charts to S3...'
 aws s3 sync . s3://$S3_BUCKET_NAME/helmchart --exclude '*' --include "*.tgz" --include "index.yaml"
