926e19130cae43ac07bc33e54dc1a877995379df
abhidp <abhi358@gmail.com>
2018-12-13 14:20:44 +1100

    Orca-Reorganising describe blocks in spec files

--- test/orca/actions/experienceTemplate.js
+++ test/orca/actions/experienceTemplate.js
@@ -5,7 +5,7 @@ export function createExperienceTemplate(responseData) {
   const data = {
     query:
       'mutation createExperienceTemplate($input: CreateExperienceTemplateInput!) { createExperienceTemplate(input: $input) { template { id key name thumbnailUrl rowVersion properties{key}}}}',
-    operationName: "createExperienceTemplate", 
+    operationName: "createExperienceTemplate",
     variables: {
       input: {
         fields: {
@@ -22,6 +22,7 @@ export function createExperienceTemplate(responseData) {
     data: data
   };
   return post(any, responseData).then(response => {
+    console.log("***********" + JSON.stringify(response));
     responseData.experienceName = name;
     responseData.expTemplateID = response.response.body.data.createExperienceTemplate.template.id;
     responseData.expTemplateRowVersion = response.response.body.data.createExperienceTemplate.template.rowVersion;
@@ -34,7 +35,7 @@ export function updateExperienceTemplate(responseData) {
   const data = {
     query:
       'mutation updateExperienceTemplate($input: UpdateExperienceTemplateInput!) { updateExperienceTemplate(input: $input) { template { id name key thumbnailUrl rowVersion properties{key}}}}',
-    operationName: "updateExperienceTemplate", 
+    operationName: "updateExperienceTemplate",
     variables: {
       input: {
         fields: {
@@ -63,7 +64,7 @@ export function getExperienceTemplate(responseData) {
   const data = {
     query:
       'query experienceTemplate($organizationId: ID!, $spaceId: ID!, $templateId: ID!) { experienceTemplate(organizationId: $organizationId , spaceId: $spaceId, templateId: $templateId) { id name key thumbnailUrl rowVersion properties{key} }}',
-    operationName: "experienceTemplate", 
+    operationName: "experienceTemplate",
     variables: {
       organizationId: responseData.orgID,
       spaceId: responseData.spaceID,
@@ -83,7 +84,7 @@ export function getExperiencesTemplate(responseData) {
   const data = {
     query:
       'query experienceTemplates($organizationId: ID!, $spaceId: ID!) { experienceTemplates(organizationId: $organizationId , spaceId: $spaceId) {templates{ id rowVersion key name}}}',
-    operationName: "experienceTemplates", 
+    operationName: "experienceTemplates",
     variables: {
       organizationId: responseData.orgID,
       spaceId: responseData.spaceID
@@ -102,7 +103,7 @@ export function deleteExperienceTemplate(responseData) {
   const data = {
     query:
       'mutation deleteExperienceTemplate($input: DeleteExperienceTemplateInput!) { deleteExperienceTemplate(input: $input) {templateId}}',
-    operationName: "deleteExperienceTemplate", 
+    operationName: "deleteExperienceTemplate",
     variables: {
       input: {
         organizationId: responseData.orgID,
--- test/orca/specs/experienceTemplateTest.js
+++ test/orca/specs/experienceTemplateTest.js
@@ -27,69 +27,49 @@ describe(' Tests for experience templates for a space', () => {
     await login(experienceTemplateObject);
     await getOrganizations(experienceTemplateObject);
     await createSpace(experienceTemplateObject);
+    createExperienceResponse = await createExperienceTemplate(experienceTemplateObject);
   });
-  describe('Mutation - createExperienceTemplate', () => {
-    before(async () => {
-      createExperienceResponse = await createExperienceTemplate(experienceTemplateObject);
-    });
-    it('Create new experience', () => {
-      expect(createExperienceResponse.response.statusCode).to.equal(200);
-      joi.assert(
-        createExperienceResponse.response.body.data.createExperienceTemplate.template,
-        experienceTemplateSchema(experienceTemplateObject.experienceName, experienceTemplateObject)
-      );
-    });
+  it('Mutation - createExperienceTemplate', async () => {
+    expect(createExperienceResponse.response.statusCode).to.equal(200);
+    joi.assert(
+      createExperienceResponse.response.body.data.createExperienceTemplate.template,
+      experienceTemplateSchema(experienceTemplateObject.experienceName, experienceTemplateObject)
+    );
   });
 
-  describe('Mutation - updateExperienceTemplate', () => {
-    before(async () => {
-      updateExperienceResponse = await updateExperienceTemplate(experienceTemplateObject);
-    });
-    it('Update experience', () => {
-      expect(updateExperienceResponse.response.statusCode).to.equal(200);
-      joi.assert(
-        updateExperienceResponse.response.body.data.updateExperienceTemplate.template,
-        experienceTemplateSchema(experienceTemplateObject.experienceNewName, experienceTemplateObject)
-      );
-    });
+  it('Mutation - updateExperienceTemplate', async () => {
+    updateExperienceResponse = await updateExperienceTemplate(experienceTemplateObject);
+    expect(updateExperienceResponse.response.statusCode).to.equal(200);
+    joi.assert(
+      updateExperienceResponse.response.body.data.updateExperienceTemplate.template,
+      experienceTemplateSchema(experienceTemplateObject.experienceNewName, experienceTemplateObject)
+    );
   });
 
-  describe('Query - experienceTemplate', () => {
-    before(async () => {
-      experienceTemplateResponse = await getExperienceTemplate(experienceTemplateObject);
-    });
-    it('Get experience template details', () => {
-      expect(experienceTemplateResponse.response.statusCode).to.equal(200);
-      joi.assert(
-        experienceTemplateResponse.response.body.data.experienceTemplate,
-        experienceTemplateSchema(experienceTemplateObject.experienceNewName, experienceTemplateObject)
-      );
-    });
+  it('Query - experienceTemplate', async () => {
+    experienceTemplateResponse = await getExperienceTemplate(experienceTemplateObject);
+    expect(experienceTemplateResponse.response.statusCode).to.equal(200);
+    joi.assert(
+      experienceTemplateResponse.response.body.data.experienceTemplate,
+      experienceTemplateSchema(experienceTemplateObject.experienceNewName, experienceTemplateObject)
+    );
   });
 
-  describe('Query - experienceTemplates', () => {
-    before(async () => {
-      experienceTemplatesResponse = await getExperiencesTemplate(experienceTemplateObject);
-    });
-    it('Get all experience templates in a space', () => {
-      expect(experienceTemplatesResponse.response.statusCode).to.equal(200);
-      joi.assert(
-        experienceTemplatesResponse.response.body.data.experienceTemplates,
-        getExperiencesTemplateSchema(experienceTemplateObject)
-      );
-    });
+  it('Query - experienceTemplates', async () => {
+    experienceTemplatesResponse = await getExperiencesTemplate(experienceTemplateObject);
+    expect(experienceTemplatesResponse.response.statusCode).to.equal(200);
+    joi.assert(
+      experienceTemplatesResponse.response.body.data.experienceTemplates,
+      getExperiencesTemplateSchema(experienceTemplateObject)
+    );
   });
 
-  describe('Mutation - deleteExperienceTemplate', () => {
-    before(async () => {
-      deleteExperienceResponse = await deleteExperienceTemplate(experienceTemplateObject);
-    });
-    it('Delete an experience template', () => {
-      expect(deleteExperienceResponse.response.statusCode).to.equal(200);
-      joi.assert(
-        experienceTemplatesResponse.response.body.data.deleteExperienceTemplate,
-        deleteExperiencesTemplateSchema(experienceTemplateObject)
-      );
-    });
+  it('Mutation - deleteExperienceTemplate', async () => {
+    deleteExperienceResponse = await deleteExperienceTemplate(experienceTemplateObject);
+    expect(deleteExperienceResponse.response.statusCode).to.equal(200);
+    joi.assert(
+      experienceTemplatesResponse.response.body.data.deleteExperienceTemplate,
+      deleteExperiencesTemplateSchema(experienceTemplateObject)
+    );
   });
 });
