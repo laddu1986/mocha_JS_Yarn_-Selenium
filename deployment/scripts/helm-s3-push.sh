33c6bcbb39081ad6d6478ce40a0d55c92d081d11
Abhi Daspatnaik <abhijeet.daspatnaik@massive.co>
2018-12-13 14:54:48 +1100

    nb

--- deployment/scripts/helm-s3-push.sh
+++ deployment/scripts/helm-s3-push.sh
@@ -1,10 +1,10 @@
 helm init --client-only
 
 echo -e '\nPackaging Helm chart ...'
-helm package --version 0.1.0 deployment/nightlyregression
+helm package --version 0.1.0 deployment/web-regression-test
 
 echo -e '\nCreating index.yaml file ...'
-helm repo index --url https://s3-$AWS_REGION.amazonaws.com/$S3_BUCKET_NAME/nightlyregression --home . .
+helm repo index --url https://s3-$AWS_REGION.amazonaws.com/$S3_BUCKET_NAME/mtribes-helm-charts/web-regression-test --home . .
 
 echo -e '\nSyncing index.yaml and packaged charts to S3...'
-aws s3 sync . s3://$S3_BUCKET_NAME/nightlyregression --exclude "*" --include "*.tgz" --include "index.yaml"
+aws s3 sync . s3://$S3_BUCKET_NAME/mtribes-helm-charts/web-regression-test --exclude "*" --include "*.tgz" --include "index.yaml"
