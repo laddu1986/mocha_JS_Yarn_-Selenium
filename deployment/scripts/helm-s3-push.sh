107cbff6844640ff5bd1e676c461d0cbf35b2754
abhidp <abhi358@gmail.com>
2018-12-13 15:03:37 +1100

    s3push

--- deployment/scripts/helm-s3-push.sh
+++ deployment/scripts/helm-s3-push.sh
@@ -4,7 +4,7 @@ echo -e '\nPackaging Helm chart ...'
 helm package --version 0.1.0 deployment/web-regression-test
 
 echo -e '\nCreating index.yaml file ...'
-helm repo index --url https://s3-$AWS_REGION.amazonaws.com/$S3_BUCKET_NAME/mtribes-helm-charts/web-regression-test --home . .
+helm repo index --url https://s3-$AWS_REGION.amazonaws.com/$S3_BUCKET_NAME/web-regression-test --home . .
 
 echo -e '\nSyncing index.yaml and packaged charts to S3...'
-aws s3 sync . s3://$S3_BUCKET_NAME/mtribes-helm-charts/web-regression-test --exclude "*" --include "*.tgz" --include "index.yaml"
+aws s3 sync . s3://$S3_BUCKET_NAME/web-regression-test --exclude "*" --include "*.tgz" --include "index.yaml"
