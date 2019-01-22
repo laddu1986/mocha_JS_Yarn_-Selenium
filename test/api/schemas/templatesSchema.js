6f9eef57351c523c5cd0105b492706527f1f0ebd
abhidp <abhi358@gmail.com>
2019-01-22 14:13:35 +1100

    Adding test for getProperty service call and verify the schema for each property .

--- test/api/schemas/templatesSchema.js
+++ test/api/schemas/templatesSchema.js
@@ -65,3 +65,124 @@ export function templatesSchema(templateData) {
     experienceTemplates: joi.array().items(templateSchema(templateData))
   });
 }
+const textRuleTypesObject = joi.object().keys({
+  key: joi.valid('required', 'rangeInt', 'regex').required(),
+  name: joi.valid('Required', 'Min/Max', 'Regex').required()
+});
+const intRuleTypesObject = joi.object().keys({
+  key: joi.valid('required', 'rangeInt', 'rangeIntSlider').required(),
+  name: joi.valid('Required', 'Min/Max', 'Min/Max Slider').required()
+});
+const dateRuleTypesObject = joi.object().keys({
+  key: joi.valid('required', 'rangeDate').required(),
+  name: joi.valid('Required', 'Min/Max Date').required()
+});
+const reqRuleTypesObject = joi.object().keys({
+  key: joi.valid('required').required(),
+  name: joi.valid('Required').required()
+});
+function commonAppearanceObject(value) {
+  let nameVal = value[0].toUpperCase() + value.substring(1);
+  const textApperanceObject = joi.object().keys({
+    key: joi.valid(value).required(),
+    name: joi.valid(nameVal).required(),
+    isDefault: joi.valid(true).required()
+  });
+  return textApperanceObject;
+}
+const intApperanceObject = joi.object().keys({
+  key: joi.valid('number', 'slider').required(),
+  name: joi.valid('Number', 'Slider').required(),
+  isDefault: joi.valid().when('key', { is: 'number', then: joi.valid(true).required() }), // eslint-disable-line
+  isDefault: joi.valid().when('key', { is: 'slider', then: joi.valid(false).required() }) // eslint-disable-line
+});
+
+export function getPropertySchema() {
+  let typesUppercase = [];
+  typesUppercase = types.map(a => a.charAt(0).toUpperCase() + a.substr(1));
+  typesUppercase[1] = 'Switch';
+  return joi.object().keys({
+    propertyTypes: joi
+      .array()
+      .length(6)
+      .items(
+        joi.object().keys({
+          key: joi.valid(types),
+          name: joi.valid(typesUppercase),
+          iconUrl: joi.valid('TextIcon', 'IntegerIcon', 'ToggleIcon').required(),
+          appearances: joi.array().when('key', {
+            is: constants.TemplateProperties.Types.text,
+            then: joi
+              .array()
+              .items(commonAppearanceObject(constants.TemplateProperties.Types.text))
+              .required()
+          }),
+          /*eslint-disable */
+          appearances: joi.array().when('key', {
+            is: constants.TemplateProperties.Types.int,
+            then: joi
+              .array()
+              .items(intApperanceObject)
+              .required()
+          }),
+          appearances: joi.array().when('key', {
+            is: constants.TemplateProperties.Types.bool,
+            then: joi
+              .array()
+              .items(commonAppearanceObject(constants.TemplateProperties.Types.bool))
+              .required()
+          }),
+          appearances: joi.array().when('key', {
+            is: constants.TemplateProperties.Types.color,
+            then: joi
+              .array()
+              .items(commonAppearanceObject(constants.TemplateProperties.Types.color))
+              .required()
+          }),
+          appearances: joi.array().when('key', {
+            is: constants.TemplateProperties.Types.list,
+            then: joi
+              .array()
+              .items(commonAppearanceObject(constants.TemplateProperties.Types.list))
+              .required()
+          }),
+          appearances: joi.array().when('key', {
+            is: constants.TemplateProperties.Types.date,
+            then: joi
+              .array()
+              .items(commonAppearanceObject(constants.TemplateProperties.Types.date))
+              .required()
+          }),
+          ruleTypes: joi.array().when('key', {
+            is: (constants.TemplateProperties.Types.text, constants.TemplateProperties.Types.list),
+            then: joi
+              .array()
+              .items(textRuleTypesObject)
+              .required()
+          }),
+          ruleTypes: joi.array().when('key', {
+            is: constants.TemplateProperties.Types.int,
+            then: joi
+              .array()
+              .items(intRuleTypesObject)
+              .required()
+          }),
+          ruleTypes: joi.array().when('key', {
+            is: (constants.TemplateProperties.Types.color, constants.TemplateProperties.Types.bool),
+            then: joi
+              .array()
+              .items(reqRuleTypesObject)
+              .required()
+          }),
+          ruleTypes: joi.array().when('key', {
+            is: constants.TemplateProperties.Types.date,
+            then: joi
+              .array()
+              .items(dateRuleTypesObject)
+              .required()
+          })
+          /*eslint-enable */
+        })
+      )
+  });
+}
