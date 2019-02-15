b66a7b200d7001affb1c0908f8a5d60074e0f6e2
Paul Sanchez <paul.sanchez@massive.co>
2019-02-15 11:22:29 +1100

    identities refactor

--- test/api/specs/identityTest.js
+++ test/api/specs/identityTest.js
@@ -1,38 +1,24 @@
 import { joi, Tags } from '../common';
 import * as identity from 'actions/identity';
 import * as schemas from 'schemas/identitySchema';
-var addResponse, getResponse, deleteResponse;
 
 const identityData = new Object();
 
 describe('Identity Api', () => {
-  describe(`POST /identities ${Tags.smokeTest}`, () => {
-    before(async () => {
-      addResponse = await identity.postIdentity(identityData);
-    });
-
-    it('C1295523 Add a new user identity.', () => {
-      expect(addResponse).to.have.status(201);
-      joi.assert(addResponse.body, schemas.postIdentitySchema(identityData));
-    });
+  it(`C1295523 POST /identities adds a new user identity ${Tags.smokeTest}`, async () => {
+    let addResponse = await identity.postIdentity(identityData);
+    expect(addResponse).to.have.status(201);
+    joi.assert(addResponse.body, schemas.postIdentitySchema(identityData));
   });
 
-  describe('GET /identities/{id}', () => {
-    before(async () => {
-      getResponse = await identity.getIdentityById(identityData);
-    });
-    it('C1295524 Get a identity by its id.', () => {
-      expect(getResponse).to.have.status(200);
-      joi.assert(getResponse.body, schemas.getIdentitySchema(identityData));
-    });
+  it('C1295524 GET /identities/{id} returns an identity', async () => {
+    let getResponse = await identity.getIdentityById(identityData);
+    expect(getResponse).to.have.status(200);
+    joi.assert(getResponse.body, schemas.getIdentitySchema(identityData));
   });
 
-  describe('Delete /identities/{id}', () => {
-    before(async () => {
-      deleteResponse = await identity.deleteIdentityById(identityData);
-    });
-    it('C1295525 Delete an identity by its id.', () => {
-      expect(deleteResponse).to.have.status(204);
-    });
+  it('C1295525 DELETE /identities/{id} deletes an identity', async () => {
+    let deleteResponse = await identity.deleteIdentityById(identityData);
+    expect(deleteResponse).to.have.status(204);
   });
 });
