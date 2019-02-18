539c92c792fc22ffd7097c569cc62210eaef3f89
Abhi Daspatnaik <abhijeet.daspatnaik@massive.co>
2019-02-18 18:11:41 +1100

    Revert "helm"
    This reverts commit a5f8d88751fe9b9cfb127c832c34579f3be57cbb.

--- deployment/scripts/helm-s3-push.sh
+++ deployment/scripts/helm-s3-push.sh
@@ -1,14 +1,20 @@
 helm init --client-only
-AWS_REGION=ap-southeast-2
-S3_BUCKET_NAME=mtribes-helm-charts
 
 echo -e '\nPackaging Helm charts ...'
 helm package --version 0.1.0 deployment/web-regression-test
 helm package --version 0.1.0 deployment/api-regression-test
 helm package --version 0.1.0 deployment/orca-regression-test
 
-echo -e '\nCreating index.yaml file ...'
-helm repo index --url https://s3-$AWS_REGION.amazonaws.com/$S3_BUCKET_NAME/qa-regression-test --home . . 
+echo -e '\nCreating Web index.yaml file...'
+helm repo index --url https://s3-$AWS_REGION.amazonaws.com/$S3_BUCKET_NAME/web-regression-test --home . .
 
-echo -e '\nSyncing files to S3 ...' \
- && aws s3 sync . s3://$S3_BUCKET_NAME/qa-regression-test --exclude "*" --include "*regression-test*.tgz" --include "index.yaml"
+echo -e '\nSyncing Web charts and index... Generating creating API index'
+aws s3 sync . s3://$S3_BUCKET_NAME/web-regression-test --exclude "*" --include "web*.tgz" --include "index.yaml" \
+  && helm repo index --url https://s3-$AWS_REGION.amazonaws.com/$S3_BUCKET_NAME/api-regression-test --home . .
+
+echo -e '\nSyncing API charts and index... Generating creating Orca index'
+aws s3 sync . s3://$S3_BUCKET_NAME/api-regression-test --exclude "*" --include "api*.tgz" --include "index.yaml" \
+  && helm repo index --url https://s3-$AWS_REGION.amazonaws.com/$S3_BUCKET_NAME/orca-regression-test --home . .
+
+echo -e '\nSyncing API charts and index'
+aws s3 sync . s3://$S3_BUCKET_NAME/orca-regression-test --exclude "*" --include "orca*.tgz" --include "index.yaml"
