ee27ae0cfe64d47d458f6677513f5913497181b4
abhidp <abhi358@gmail.com>
2019-02-18 16:48:25 +1100

    Replacing repetetive UI tasks with API Calls- part2

--- test/web/specs/organizations/createOrganizationTest.js
+++ test/web/specs/organizations/createOrganizationTest.js
@@ -1,26 +1,30 @@
 // Create Organization, sign out, sign back in to validate user lands in the created Org
 import * as lib from '../../common';
-import { createAccount } from 'actions/account';
-import { signIn } from 'actions/common';
+import { signIn, postIdentity, postOrganization, postMembership } from 'actions/common';
 import { signOut } from 'actions/navBar';
 import {
   verifyOrgNameOnDashBoard,
   goToCreateOrgPageFromNavbar,
   verifyCreateOrgPage,
   createNewOrg,
-  verifyWecomeOrgPage
+  verifyWecomeOrgPage,
+  selectOrg
 } from 'actions/organization';
 import { getnavOrgCount } from 'actions/navBar';
-import accountPage from 'page_objects/accountPage';
-var orgName = lib.randomString.generate(10),
-  accountDetails;
-
+import SignInPage from 'page_objects/signInPage';
+var orgName = lib.randomString.generate(10);
+const accountData = new Object();
 describe('Tests for Create Organization', () => {
-  before(() => {
-    accountPage.open();
-    accountDetails = createAccount();
+  before(async () => {
+    await postIdentity(accountData);
+    await postOrganization(accountData);
+    await postMembership(accountData);
   });
-
+  before(() => {
+    SignInPage.open();
+    signIn(accountData.identityEmail, process.env.ACCOUNT_PASS);
+    selectOrg();
+  });
   it(`C1295698 Go to organization creation page ${lib.Tags.smokeTest}`, () => {
     goToCreateOrgPageFromNavbar();
     expect(verifyCreateOrgPage()).to.equal(true);
@@ -37,7 +41,7 @@ describe('Tests for Create Organization', () => {
 
   it('C1295700 Sign out and back in -->Should show last accessed Org dashboard', () => {
     signOut();
-    signIn(accountDetails.email, process.env.ACCOUNT_PASS);
+    signIn(accountData.identityEmail, process.env.ACCOUNT_PASS);
     expect(verifyOrgNameOnDashBoard()).to.equal(orgName);
   });
 
