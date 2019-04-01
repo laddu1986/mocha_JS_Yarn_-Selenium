7e02ced56ba0abd271e8f73dac9fc4c7602d3d2e
abhidp <abhi358@gmail.com>
2019-04-01 13:20:15 +1100

    Property Test cases - complete

--- test/api/actions/templates.js
+++ test/api/actions/templates.js
@@ -88,7 +88,9 @@ export function deleteExperienceTemplate(templateData) {
 export function getPropertyById(templateData) {
   const req = new readClient.Request('getPropertyById', {
     context: spaceContext(templateData),
-    templateId: templateData.template.templateId
+    templateId: templateData.template.templateId,
+    propertyId: templateData.template.propertyId,
+    templateVersionId: templateData.template.templateVersionId
   }).withResponseStatus(true);
   return req.exec();
 }
